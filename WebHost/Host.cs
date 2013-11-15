dfe30fc36dd0ccb12dfa80983f921e359452d1fa
David Fowler <davidfowl@gmail.com>
2013-11-15 04:25:35 -0800

    Fix the tracing for web host.

--- WebHost/Host.cs
+++ WebHost/Host.cs
@@ -70,6 +70,10 @@ namespace Microsoft.Owin.Hosting.Starter
                 Environment.Exit(250);
             };
 
+            var listener = new ConsoleTraceListener();
+            Trace.Listeners.Add(listener);
+            Trace.AutoFlush = true;
+
             try
             {
                 var assembly = _host.Run();
@@ -93,7 +97,7 @@ namespace Microsoft.Owin.Hosting.Starter
                     throw;
                 }
 
-                Console.Error.WriteLine(inner.Message);
+                Trace.TraceError(inner.Message);
             }
         }
 
