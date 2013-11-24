cfedafb2a79eeb18fe1199bf6e24b8ecd08017b0
Louis DeJardin <lodejard@microsoft.com>
2013-11-23 22:13:48 -0800

    Repairing ms.net.web

--- src/Microsoft.Net.OwinHost/MyAppLoader.cs
+++ src/Microsoft.Net.OwinHost/MyAppLoader.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Loader;
+using Microsoft.Net.Runtime;
 using Microsoft.Owin.Hosting.Loader;
 using Owin;
 
--- src/Microsoft.Net.OwinHost/Properties/AssemblyInfo.cs
+++ src/Microsoft.Net.OwinHost/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebHost")]
+[assembly: AssemblyTitle("Microsoft.Net.OwinHost")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebHost")]
+[assembly: AssemblyProduct("Microsoft.Net.OwinHost")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
