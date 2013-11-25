14539c45b7b7e740cf21fada8cd6a10bc73b7094
David Fowler <davidfowl@gmail.com>
2013-11-25 00:24:37 -0800

    Fixing namespaces.

--- src/Microsoft.Net.ApplicationHost/Program.cs
+++ src/Microsoft.Net.ApplicationHost/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Reflection;
 using Microsoft.Net.Runtime;
 
-namespace K
+namespace Microsoft.Net.ApplicationHost
 {
     public class Program
     {
--- src/Microsoft.Net.Project/Program.cs
+++ src/Microsoft.Net.Project/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Reflection;
 using Microsoft.Net.Runtime;
 
-namespace K
+namespace Microsoft.Net.Project
 {
     public class Program
     {
