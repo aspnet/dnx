7451f59db3710ed1d4cc1d885598f92a23621c13
David Fowler <davidfowl@gmail.com>
2013-11-29 21:14:13 -0800

    Removed echo from klr

--- scripts/klr.cmd
+++ scripts/klr.cmd
@@ -5,7 +5,7 @@ IF "%K_APPBASE%" NEQ "" (
   SET K_OPTIONS=%K_OPTIONS% --appbase %K_APPBASE%
 )
 
-echo %~dp0..\bin\Debug\klr.exe %K_OPTIONS% %*
+::echo %~dp0..\bin\Debug\klr.exe %K_OPTIONS% %*
 
 %~dp0..\bin\Debug\klr.exe %K_OPTIONS% %*
 ENDLOCAL
