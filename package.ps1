a50ec78255c19486c8666211f33a717572dfd304
David Fowler <davidfowl@gmail.com>
2013-11-29 20:16:27 -0800

    Added nuspec and nuget pack step.

--- package.ps1
+++ package.ps1
@@ -1,15 +1,17 @@
 param($configuration = "Debug", $includeSymbols = $false)
 
+$sdkRoot = "artifacts\sdk"
+
 .\build.ps1 $configuration
 
-mkdir artifacts -force | Out-Null
+mkdir $sdkRoot -force | Out-Null
 
-mkdir artifacts\tools -force | Out-Null
+mkdir $sdkRoot\tools -force | Out-Null
 
-cp scripts\* artifacts\tools
+cp scripts\* $sdkRoot\tools
 
 # Fixup scripts
-$scripts = ls artifacts\tools -filter *.cmd
+$scripts = ls $sdkRoot\tools -filter *.cmd
 
 foreach($file in $scripts)
 {
@@ -19,32 +21,36 @@ foreach($file in $scripts)
 }
 
 # Manually fixup k.cmd to use compiled version of MS.Net.Project
-$content = cat artifacts\tools\k.cmd | %{ $_.Replace("%~dp0k-run %~dp0Microsoft.Net.Project %*", "CALL %~dp0KLR %~dp0Microsoft.Net.Project\bin\$configuration\Microsoft.Net.Project.dll %*") }
-Set-Content artifacts\tools\k.cmd $content
+$content = cat $sdkRoot\tools\k.cmd | %{ $_.Replace("%~dp0k-run %~dp0Microsoft.Net.Project %*", "CALL %~dp0KLR %~dp0Microsoft.Net.Project\bin\$configuration\Microsoft.Net.Project.dll %*") }
+Set-Content $sdkRoot\tools\k.cmd $content
 
 # Move the bin folder in here
-mkdir artifacts\tools\bin -force | Out-Null
-cp bin\$configuration\* artifacts\tools\bin\
+mkdir $sdkRoot\tools\bin -force | Out-Null
+cp bin\$configuration\* $sdkRoot\tools\bin\
 
 if(!$includeSymbols)
 {
     # Remove the stuff we don't need
-    ls artifacts\tools\bin\*.pdb | rm
-    ls artifacts\tools\bin\*.ilk | rm
-    ls artifacts\tools\bin\*.lib | rm
-    ls artifacts\tools\bin\*.exp | rm
+    ls $sdkRoot\tools\bin\*.pdb | rm
+    ls $sdkRoot\tools\bin\*.ilk | rm
+    ls $sdkRoot\tools\bin\*.lib | rm
+    ls $sdkRoot\tools\bin\*.exp | rm
 }
 
 # Now copy source
-cp -r src\Microsoft.Net.ApplicationHost artifacts\tools -Force
-cp -r src\Microsoft.Net.Project artifacts\tools -Force
-cp -r src\Microsoft.Net.OwinHost artifacts\tools -Force
-cp -r src\Microsoft.Net.Launch artifacts\tools -Force
-cp -r src\Microsoft.Net.Runtime artifacts\tools -Force
+cp -r src\Microsoft.Net.ApplicationHost $sdkRoot\tools -Force
+cp -r src\Microsoft.Net.Project $sdkRoot\tools -Force
+cp -r src\Microsoft.Net.OwinHost $sdkRoot\tools -Force
+cp -r src\Microsoft.Net.Launch $sdkRoot\tools -Force
+cp -r src\Microsoft.Net.Runtime $sdkRoot\tools -Force
 
 if(!$includeSymbols)
 {
     # Remove the stuff we don't need
-    ls -r artifacts\tools\*obj | rm -r
-    ls -r artifacts\tools\*\bin\*\*.xml | rm
+    ls -r $sdkRoot\tools\*obj | rm -r
+    ls -r $sdkRoot\tools\*\bin\*\*.xml | rm
 }
+
+# NuGet pack
+cp ProjectK.nuspec $sdkRoot
+.nuget\NuGet.exe pack $sdkRoot\ProjectK.nuspec -o $sdkRoot -NoPackageAnalysis
