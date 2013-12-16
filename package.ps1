056826fad8f16732c955c2d9e4d0ed662bd69173
David Fowler <davidfowl@gmail.com>
2013-12-15 16:42:20 -0800

    Made some modifications to the script.

--- package.ps1
+++ package.ps1
@@ -1,18 +1,25 @@
-param($configuration = "Debug", $includeSymbols = $false, $runtimePath = $env:RuntimePath, $nightly = $false)
+param($configuration = "Debug", $runtimePath = $env:RuntimePath, $nightly = $false)
 
 $ErrorActionPreference = "Stop"
 
+# Get the executing script path
 $path = Split-Path $MyInvocation.MyCommand.Path
 
+# Root of the sdk
 $sdkRoot = "$path\artifacts\sdk"
 
-mkdir $sdkRoot -force | Out-Null
+# The list of projects we're interested in harvesting
+$hostProjects = @("Microsoft.Net.Runtime.Interfaces", "klr.host", "Stubs")
+$runtimeProjects = @("Microsoft.Net.OwinHost", "Microsoft.Net.Runtime", "Microsoft.Net.Project", "Microsoft.Net.ApplicationHost")
 
+# Make the sdk and tools folders
+mkdir $sdkRoot -force | Out-Null
 mkdir $sdkRoot\tools -force | Out-Null
 
+# Copy the cmd files
 cp $path\scripts\* $sdkRoot\tools
 
-# Fixup scripts
+# Fixup scripts based on new relative paths in the resulting package
 $scripts = ls $sdkRoot\tools -filter *.cmd
 
 foreach($file in $scripts)
@@ -26,62 +33,55 @@ foreach($file in $scripts)
 mkdir $sdkRoot\tools\bin -force | Out-Null
 cp $path\bin\$configuration\* $sdkRoot\tools\bin\
 
+# If we're building ourselves this will be true
 $bootstrapping = $false
 
-# if the framework is local then build it against the frameworks
+# if the framework is local then build ourselves
 if((Test-Path $sdkRoot\Framework) -and (Test-Path $sdkRoot\tools\Microsoft.Net.Project))
 {
-    & $sdkRoot\tools\k.cmd build $path\src\Microsoft.Net.Runtime.Interfaces
-    & $sdkRoot\tools\k.cmd build $path\src\klr.host
-    & $sdkRoot\tools\k.cmd build $path\src\Stubs
+    # tools\bin\*
+    $hostProjects | %{
+        & $sdkRoot\tools\k.cmd build $path\src\$_
+        cp -r $path\src\$_ -filter *.dll $sdkRoot\tools\bin\ -force
+
+        # Remove this file from the root if it exists there
+        if(Test-Path $sdkRoot\tools\bin\$_.dll) {
+            rm $sdkRoot\tools\bin\$_.dll
+        }
+    }
     
-    # Stuff in the bin folder
-    cp -r $path\src\Microsoft.Net.Runtime.Interfaces -filter *.dll $sdkRoot\tools\bin\ -force
-    cp -r $path\src\klr.host -filter *.dll $sdkRoot\tools\bin\ -force
-    cp -r $path\src\Stubs -filter *.dll $sdkRoot\tools\bin\ -force
-    
-    # Stuff one level up (TODO: OwinHost)
-    & $sdkRoot\tools\k.cmd build $path\src\Microsoft.Net.Runtime
-    & $sdkRoot\tools\k.cmd build $path\src\Microsoft.Net.Project
-    & $sdkRoot\tools\k.cmd build $path\src\Microsoft.Net.ApplicationHost
-    
-    rm $sdkRoot\tools\bin\klr.host.dll
-    rm $sdkRoot\tools\bin\Microsoft.Net.Runtime.Interfaces.dll
-    rm -r -force $sdkRoot\tools\bin\*\bin\Debug
-    rm -r -force $sdkRoot\tools\bin\*\Properties
-    rm -r -force $sdkRoot\tools\bin\*\obj
-    
+    # tools\* Skip OwinHost since it isn't buildable yet
+    $runtimeProjects | Select -Skip 1 | %{
+        & $sdkRoot\tools\k.cmd build $path\src\$_
+    }
+
     $bootstrapping = $true
 }
 
-if(!$includeSymbols)
-{
-    # Remove the stuff we don't need
-    ls $sdkRoot\tools\bin\*.pdb | rm
-    ls $sdkRoot\tools\bin\*.ilk | rm
-    ls $sdkRoot\tools\bin\*.lib | rm
-    ls $sdkRoot\tools\bin\*.exp | rm
+# Copy the binaries to tools\*
+$runtimeProjects | %{
+    cp -r $path\src\$_ -filter *.dll $sdkRoot\tools -Force    
 }
 
-# Now copy source
-cp -r $path\src\Microsoft.Net.ApplicationHost -filter *.dll $sdkRoot\tools -Force
-cp -r $path\src\Microsoft.Net.Project -filter *.dll $sdkRoot\tools -Force
-cp -r $path\src\Microsoft.Net.OwinHost -filter *.dll $sdkRoot\tools -Force
+# Temporary special case for Microsoft.Net.Launch
 cp -r $path\src\Microsoft.Net.Launch $sdkRoot\tools -Force
-cp -r $path\src\Microsoft.Net.Runtime -filter *.dll $sdkRoot\tools -Force
 cp $path\src\Microsoft.Net.Runtime\Executable.cs $sdkRoot\tools\Microsoft.Net.Launch -Force
 rm $sdkRoot\tools\Microsoft.Net.Launch\.include
 
 # If we're bootstrapping then do some extra steps
 if($bootstrapping)
 {
-    # Copy dependencies to app host an family
-    @("Microsoft.Net.Project", "Microsoft.Net.ApplicationHost") | %{
+    $runners = @("Microsoft.Net.Project", "Microsoft.Net.ApplicationHost")
+    $targetFrameworks = @("k10", "net45")
+    $packages = @("Newtonsoft.Json", "Microsoft.CodeAnalysis", "Microsoft.CodeAnalysis.CSharp", "System.Collections.Immutable", "System.Reflection.Metadata.Ecma335")
+
+    # Copy the dependency graph to each runner folder
+    $runners | %{
         $project = $_
-        @("k10", "net45") | %{
+        $targetFrameworks | %{
             $framework = $_
             cp $sdkRoot\tools\Microsoft.Net.Runtime\bin\$framework\Microsoft.Net.Runtime.dll $sdkRoot\tools\$project\bin\$framework
-            @("Newtonsoft.Json", "Microsoft.CodeAnalysis", "Microsoft.CodeAnalysis.CSharp", "System.Collections.Immutable", "System.Reflection.Metadata.Ecma335") | %{
+            $packages | %{
                 $package = $_
                 ls $path\packages\$package.*\lib\$framework\$package.dll | %{ cp $_.FullName $sdkRoot\tools\$project\bin\$framework }
             }
@@ -89,9 +89,19 @@ if($bootstrapping)
     }
 }
 
-# Remove the stuff we don't need
-rm -r $sdkRoot\tools\*\obj 
-rm -r -force $sdkRoot\tools\*\properties
+# Remove files we don't care about
+$foldersToRemove = @("bin\$configuration", "obj", "properties")
+$foldersToRemove | %{
+    rm -r -force $sdkRoot\tools\bin\*\$_
+}
+
+$foldersToRemove | Select -Skip 1 | %{
+    rm -r -force $sdkRoot\tools\*\$_
+}
+
+@("pdb", "ilk", "lib", "exp") | %{
+    rm $sdkRoot\tools\bin\*.$_ | rm
+}
 
 # Copy the runtime
 if($runtimePath) 
