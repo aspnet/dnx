cb5728078fa7b2b4d24a04b2cfd73cf35dd048ed
David Fowler <davidfowl@gmail.com>
2014-01-15 00:15:05 -0800

    Fixed more nuget issues and cleaned up samples. - Removed HelloCommon and restored HelloWorld project. - Added snapshot dependency to HelloWeb

--- src/HelloCommon/Testing.cs
+++
@@ -1,8 +0,0 @@
-
-public class Testing
-{
-	public string Method()
-	{
-		return "ReturnValue";
-	}
-}
--- src/HelloWorld/Program.cs
+++ src/HelloWorld/Program.cs
@@ -3,7 +3,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-    	var returnValue = new Testing().Method();
         System.Console.WriteLine("Hello World!");
     }
 }
