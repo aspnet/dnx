631f52efa1ab239ec4eb8e1e0941f5baf934d0e9
David Fowler <davidfowl@gmail.com>
2014-03-02 13:37:14 -0800

    Add initial unit tests for assembly neutral interfaces

---
+++ test/Microsoft.Net.Runtime.Roslyn.Tests/AssemblyNeutralFacts.cs
@@ -0,0 +1,108 @@
+﻿using System.Linq;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Xunit;
+
+namespace Microsoft.Net.Runtime.Roslyn.Tests
+{
+    public class AssemblyNeutralFacts
+    {
+        [Fact]
+        public void TypeCompilationsAreGeneratedForEachAssemblyNeutralType()
+        {
+            var worker = DoAssemblyNeutralCompilation(@"
+namespace Something
+{
+    [AssemblyNeutral]
+    public interface IFoo { }
+
+    [AssemblyNeutral]
+    public interface IBar { }
+
+    [AssemblyNeutral]
+    public class AssemblyNeutralAttribute : System.Attribute { }
+}
+");
+
+            var diagnostics = worker.GenerateTypeCompilations();
+            var compilations = worker.TypeCompilations.OrderBy(c => c.AssemblyName).ToList();
+
+            Assert.Equal(0, diagnostics.Count);
+            Assert.Equal(3, compilations.Count());
+            Assert.Equal("Something.AssemblyNeutralAttribute", compilations[0].AssemblyName);
+            Assert.Equal("Something.IBar", compilations[1].AssemblyName);
+            Assert.Equal("Something.IFoo", compilations[2].AssemblyName);
+        }
+
+        [Fact]
+        public void CircularReferencesInMembers()
+        {
+            var worker = DoAssemblyNeutralCompilation(
+@"
+namespace Something
+{
+    [AssemblyNeutral]
+    public interface IHttpRequest 
+    { 
+        IHttpContext Context { get; }
+
+        string Verb { get; } 
+    }
+}",
+@"
+namespace Something
+{
+    [AssemblyNeutral]
+    public interface IHttpResponse 
+    { 
+        IHttpContext Context { get; }
+
+        void Write(string text);
+    }
+}",
+@"
+namespace Something
+{
+    [AssemblyNeutral]
+    public interface IHttpContext 
+    { 
+        IHttpRequest Request { get; set; }
+        IHttpResponse Response { get; set; }
+    }
+}",
+@"
+namespace Something
+{
+    [AssemblyNeutral]
+    public class AssemblyNeutralAttribute : System.Attribute { }
+}
+");
+
+            var diagnostics = worker.GenerateTypeCompilations();
+            var compilations = worker.TypeCompilations.OrderBy(c => c.AssemblyName).ToList();
+
+            Assert.Equal(0, diagnostics.Count);
+            Assert.Equal(4, compilations.Count());
+            Assert.Equal("Something.AssemblyNeutralAttribute", compilations[0].AssemblyName);
+            Assert.Equal("Something.IHttpContext", compilations[1].AssemblyName);
+            Assert.Equal("Something.IHttpRequest", compilations[2].AssemblyName);
+            Assert.Equal("Something.IHttpResponse", compilations[3].AssemblyName);
+        }
+
+        private AssemblyNeutralWorker DoAssemblyNeutralCompilation(params string[] fileContents)
+        {
+            var compilation = CSharpCompilation.Create("test",
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary),
+                references: new[] { 
+                    new MetadataFileReference(typeof(object).GetTypeInfo().Assembly.Location) 
+                },
+                syntaxTrees: fileContents.Select(text => CSharpSyntaxTree.ParseText(text)));
+
+            var worker = new AssemblyNeutralWorker(compilation);
+            worker.FindTypeCompilations(compilation.GlobalNamespace);
+            worker.OrderTypeCompilations();
+            return worker;
+        }
+    }
+}
