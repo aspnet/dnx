20f3598f81aa3db4f656c8af90183d4a2b7fff75
Louis DeJardin <lodejard@microsoft.com>
2014-03-02 12:51:33 -0800

    Fixing cmd scripts for local development

--- scripts/K.cmd
+++ scripts/K.cmd
@@ -1,4 +1,6 @@
 @Echo OFF
+SETLOCAL
+SET ERRORLEVEL=
 :: K [command] [args]
 ::   command :  Required - Name of the command to execute
 ::   args : Optional - Any further args will be passed directly to the command.
@@ -6,8 +8,9 @@
 :: e.g. To compile the app in the current folder:
 ::      C:\src\MyApp\>K build
 
-SETLOCAL
-SET ERRORLEVEL=
+REM <dev>
+@Echo ON
+REM </dev>
 
 IF EXIST "%~dp0k-%1.cmd" (
   "%~dp0k-%1.cmd" %2 %3 %4 %5 %6 %7 %8 %9 
--- scripts/k-build.cmd
+++ scripts/k-build.cmd
@@ -2,6 +2,10 @@
 SETLOCAL
 SET ERRORLEVEL=
 
+REM <dev>
+@Echo ON
+REM </dev>
+
 CALL "%~dp0KLR" "Microsoft.Net.Project" build %*
 
 exit /b %ERRORLEVEL%
