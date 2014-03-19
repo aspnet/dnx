13745e6baca2d365902a525df4686fcaa5283066
Pranav K <prkrishn@hotmail.com>
2014-03-19 15:31:52 -0700

    Adding ILibraryManager to Runtime assembly

---
+++ src/Microsoft.Net.Runtime/DependencyManagement/LibraryInformation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Net.Runtime
+{
+    public sealed class LibraryInformation : ILibraryInformation
+    {
+        public LibraryInformation(LibraryDescription description)
+        {
+            Name = description.Identity.Name;
+            Dependencies = description.Dependencies.Select(d => d.Name);
+        }
+
+        public LibraryInformation(string name, IEnumerable<string> dependencies)
+        {
+            Name = name;
+            Dependencies = dependencies;
+        }
+
+        public string Name
+        {
+            get;
+            private set;
+        }
+
+        public IEnumerable<string> Dependencies
+        {
+            get;
+            private set;
+        }
+    }
+}
