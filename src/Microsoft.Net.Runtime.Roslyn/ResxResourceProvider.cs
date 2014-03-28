65fcbc109d53a186ec0a996de908cafa1467625c
David Fowler <davidfowl@gmail.com>
2014-03-28 02:56:09 -0700

    Enable resx writing in core clr build.

--- src/Microsoft.Net.Runtime.Roslyn/ResxResourceProvider.cs
+++ src/Microsoft.Net.Runtime.Roslyn/ResxResourceProvider.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-#if NET45
 using System.Resources;
-#endif
 using System.Xml.Linq;
 using Microsoft.CodeAnalysis;
 
@@ -15,17 +13,13 @@ namespace Microsoft.Net.Runtime.Roslyn
     {
         public IList<ResourceDescription> GetResources(Project project)
         {
-#if NET45 // CORECLR_TODO: ResourceWriter
             return Directory.EnumerateFiles(project.ProjectDirectory, "*.resx", SearchOption.AllDirectories)
                             .Select(resxFilePath =>
                                 new ResourceDescription(GetResourceName(project.Name, resxFilePath),
                                                         () => GetResourceStream(resxFilePath),
                                                         isPublic: true)).ToList();
-#else
-            return new ResourceDescription[0];
-#endif
         }
-#if NET45 // CORECLR_TODO: ResourceWriter
+
         private static string GetResourceName(string projectName, string resxFilePath)
         {
             Trace.TraceInformation("Found resource {0}", resxFilePath);
@@ -64,7 +58,5 @@ namespace Microsoft.Net.Runtime.Roslyn
                 return ms;
             }
         }
-#endif
-
     }
 }
