8184855a48c74e1b41cc19c0cdacb08069104de0
Louis DeJardin <louis.dejardin@gmail.com>
2014-04-29 23:46:13 -0700

    Moving secondary binaries to lib\name\name.dll

---
+++ scripts/kpm.cmd
@@ -0,0 +1,1 @@
+@"%~dp0klr.exe" --lib "%~dp0;%~dp0lib\Microsoft.Net.PackageManager" Microsoft.Net.PackageManager %*
--- src/Microsoft.Net.PackageManager/IReport.cs
+++ src/Microsoft.Net.PackageManager/IReport.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Web;
 
 namespace Microsoft.Net.PackageManager
 {
