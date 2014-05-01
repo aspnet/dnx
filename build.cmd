393522c0159e69af60f2427346be6ec8a363b5e3
Louis DeJardin <louis.dejardin@gmail.com>
2014-04-30 20:57:47 -0700

    build server launches notepad with CALL ps1

--- build.cmd
+++ build.cmd
@@ -4,7 +4,7 @@ cd %~dp0
 SETLOCAL
 SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
 
-CALL setup\kvm.ps1 install
+CALL setup\kvm.cmd install
 
 IF EXIST %CACHED_NUGET% goto copynuget
 echo Downloading latest version of NuGet.exe...
