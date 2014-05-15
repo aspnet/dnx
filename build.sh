285facd73ab6ae94175a5386049a504f0a97ded7
Nobuhiro Nakamura <lefb766@live.jp>
2014-05-15 16:35:38 +0900

    Fix some faults in build.sh
    - build.sh now acquires nuget.exe properly in Linux environments
    - Hide the curl command's stderr

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 if test `uname` = Darwin; then
     cachedir=~/Library/Caches/KBuild
 else
-    if x$XDG_DATA_HOME = x; then
+    if test x$XDG_DATA_HOME = x; then
 	cachedir=$HOME/.local/share
     else
 	cachedir=$XDG_DATA_HOME;
@@ -14,7 +14,7 @@ mkdir -p $cachedir
 url=https://www.nuget.org/nuget.exe
 
 if test ! -f $cachedir/nuget.exe; then
-    wget -o $cachedir/nuget.exe $url 2>/dev/null || curl -o $cachedir/nuget.exe --location $url /dev/null
+    wget -O $cachedir/nuget.exe $url 2>/dev/null || curl -o $cachedir/nuget.exe --location $url 2>/dev/null
 fi
 
 if test ! -e .nuget; then
