aaf40aaa5990c802a9705245d6b4385ec081473d
David Fowler <davidfowl@gmail.com>
2014-05-26 01:48:29 -0700

    Clean up kpm.cmd - Made it look like the other k*.cmd files. - Turned echo off if not in dev mode. - Flow exit code through

--- scripts/kpm.cmd
+++ scripts/kpm.cmd
@@ -1,1 +1,12 @@
+@Echo OFF
+SETLOCAL
+SET ERRORLEVEL=
+
+REM <dev>
+@Echo ON
+REM </dev>
+
 CALL "%~dp0KLR.cmd" --lib "%~dp0;%~dp0lib\Microsoft.Framework.PackageManager" "Microsoft.Framework.PackageManager" %*
+
+exit /b %ERRORLEVEL%
+ENDLOCAL
