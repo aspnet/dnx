ba6ae15ae5761a66f2be718fd4ff27c70ccb14de
David Fowler <davidfowl@gmail.com>
2014-06-12 05:04:47 -0700

    Added the ability to change the loader for a specific project - This is the beginning of allowing interesting scenarios (like plugging in other languages). - The default project loader is still roslyn. - Create packages for the rest of the runtime parts

---
+++ src/Microsoft.Framework.Runtime/TargetFrameworkConfiguration.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.Versioning;
+using NuGet;
+
+namespace Microsoft.Framework.Runtime
+{
+    public class TargetFrameworkConfiguration : IFrameworkTargetable
+    {
+        public FrameworkName FrameworkName { get; set; }
+
+        public IList<Library> Dependencies { get; set; }
+
+        public IEnumerable<FrameworkName> SupportedFrameworks
+        {
+            get
+            {
+                return new[] { FrameworkName };
+            }
+        }
+    }
+}
