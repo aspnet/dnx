3f9805c87bd0649e8a2583b5c3397067dfdb36ce
David Fowler <davidfowl@gmail.com>
2014-06-23 22:23:26 -0700

    Removed unused namespace

--- src/Microsoft.Framework.Project/Program.cs
+++ src/Microsoft.Framework.Project/Program.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.IO;
-using Microsoft.Framework.PackageManager;
 using Microsoft.Framework.Runtime;
 using Microsoft.Framework.Runtime.Common.CommandLine;
 
