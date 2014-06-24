5be2c544831bf754965331d140b3c913966096b0
Graeme Christie <graeme@sx.net.au>
2014-06-24 22:24:39 +0800

    http=>HTTP

--- setup/kvm.sh
+++ setup/kvm.sh
@@ -89,7 +89,7 @@ _kvm_download() {
     local httpResult=$(curl -L -D - -u aspnetreadonly:4d8a2d9c-7b80-4162-9978-47e918c9658c "$url" -o "$kreFile" 2>/dev/null | grep "^HTTP/1.1" | head -n 1 | sed "s/HTTP.1.1 \([0-9]*\).*/\1/")
 
     [[ $httpResult == "404" ]] && echo "$kreFullName was not found in repository $KRE_NUGET_API_URL" && return 1
-    [[ $httpResult != "302" && $httpResult != "200" ]] && echo "Http Error $httpResult fetching $kreFullName from $KRE_NUGET_API_URL" && return 1
+    [[ $httpResult != "302" && $httpResult != "200" ]] && echo "HTTP Error $httpResult fetching $kreFullName from $KRE_NUGET_API_URL" && return 1
 
     _kvm_unpack $kreFile $kreFolder
 }
