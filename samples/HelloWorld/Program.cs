9791416af613a8f69786eebcca011ff2fbfca7ef
David Fowler <davidfowl@gmail.com>
2014-06-25 22:18:15 -0700

    Updated sample to get new resource path

--- samples/HelloWorld/Program.cs
+++ samples/HelloWorld/Program.cs
@@ -1,13 +1,24 @@
+using System.Reflection;
 
 public class Program
 {
-    public void Main(string[] args)
+    public int Main(string[] args)
     {
         System.Console.WriteLine("Hello World!");
+
+        var resourceStream = typeof(Program).GetTypeInfo().Assembly.GetManifestResourceStream("compiler/resources/HTMLPage1.html");
+
+        if (resourceStream == null)
+        {
+            return 1;
+        }
+        
         System.Console.WriteLine(HelloShared.HelloSharedCode.SharedMethod());
         foreach (var arg in args)
         {
             System.Console.WriteLine(arg);
         }
+
+        return 0;
     }
 }
