1ddaa76c02a877a50eb6ea44465a31cbdb76f5bb
Pranav K <prkrishn@hotmail.com>
2014-08-04 14:38:41 -0700

    Revert "Revert "Updated to handle roslyn changes""
    This reverts commit 069dfb964a86ab505dd6df5e5495434072346b6c.

--- src/Microsoft.Framework.Project/AssemblyInformation.cs
+++ src/Microsoft.Framework.Project/AssemblyInformation.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection.PortableExecutable;
+using System.Reflection.Metadata;
 
 namespace Microsoft.Framework.Project
 {
