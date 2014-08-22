8fa124e8a3c2d2a4c5e4d719a864d43c317b3033
Louis DeJardin <louis.dejardin@gmail.com>
2014-08-22 13:39:28 -0700

    One file per type for interfaces

---
+++ src/Microsoft.Framework.Runtime.Interfaces/IBeforeCompileContext.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Collections.Generic;
+
+namespace Microsoft.Framework.Runtime
+{
+    [AssemblyNeutral]
+    public interface IBeforeCompileContext
+    {
+        CSharpCompilation CSharpCompilation { get; set; }
+
+        IList<ResourceDescription> Resources { get; }
+
+        IList<Diagnostic> Diagnostics { get; }
+    }
+}
