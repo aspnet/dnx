2b610720db553b27e13e710c30bcad6b4569bcc4
Louis DeJardin <louis.dejardin@gmail.com>
2014-08-22 13:39:28 -0700

    One file per type for interfaces

--- src/Microsoft.Framework.Runtime.Interfaces/IAfterCompileContext.cs
+++ src/Microsoft.Framework.Runtime.Interfaces/IAfterCompileContext.cs
@@ -1,16 +1,18 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Collections.Generic;
+
 namespace Microsoft.Framework.Runtime
 {
-    /// <summary>
-    /// Summary description for ICompileModule
-    /// </summary>
     [AssemblyNeutral]
-    public interface ICompileModule
+    public interface IAfterCompileContext
     {
-        void BeforeCompile(IBeforeCompileContext context);
+        CSharpCompilation CSharpCompilation { get; set; }
 
-        void AfterCompile(IAfterCompileContext context);
+        IList<Diagnostic> Diagnostics { get; }
     }
 }
