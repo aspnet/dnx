e8ab99f425b6f8f4711a14db90cefc960ffeca82
shhsu <shhsu@microsoft.com>
2014-10-06 10:04:05 -0700

    Disable partial ngen when crossgening

--- src/Microsoft.Framework.Project/CrossGenManager.cs
+++ src/Microsoft.Framework.Project/CrossGenManager.cs
@@ -58,6 +58,13 @@ namespace Microsoft.Framework.Project
                 RedirectStandardOutput = true
             };
 
+            // Disable partial NGEN
+#if NET45
+            options.EnvironmentVariables["COMPLUS_PartialNGen"] = "0";
+#else
+            options.Environment["COMPLUS_PartialNGen"] = "0";
+#endif
+
             var p = Process.Start(options);
 #if NET45
             p.EnableRaisingEvents = true;
