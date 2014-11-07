40fe5bd0d55af3304820020c8fdeb1aca871b5b3
Troy Dai <troy.dai@outlook.com>
2014-11-07 14:05:57 -0800

    Enable to script block in Mono environment
    Rules:
    
    In non-mono environment, if the ComSpec exists the script block will
    wrapped in cmd /C, otherwise the first argument will be used as FileName
    for the new process.
    
    For mono environment if the given script path can be found after appending
    .sh to it then execute that script with /bin/bash. Otherwise use the
    script name as FileName for process start info.

--- src/Microsoft.Framework.PackageManager/Scripts/ScriptExecutor.cs
+++ src/Microsoft.Framework.PackageManager/Scripts/ScriptExecutor.cs
@@ -1,15 +1,16 @@
-using Microsoft.Framework.ApplicationHost.Impl.Syntax;
-using Microsoft.Framework.Runtime;
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using Microsoft.Framework.ApplicationHost.Impl.Syntax;
+using Microsoft.Framework.Runtime;
 
 namespace Microsoft.Framework.PackageManager
 {
-    /// <summary>
-    /// Summary description for ScriptExecutor
-    /// </summary>
     public class ScriptExecutor
     {
         public void Execute(Runtime.Project project, string scriptName, Func<string, string> getVariable)
@@ -26,17 +27,41 @@ namespace Microsoft.Framework.PackageManager
                     scriptCommandLine,
                     GetScriptVariable(project, getVariable));
 
-                // Command-lines on Windows are executed via "cmd /C" in order
-                // to support batch files, &&, built-in commands like echo, etc.
-                // ComSpec is Windows-specific, and contains the full path to cmd.exe
-                var comSpec = Environment.GetEnvironmentVariable("ComSpec");
-                if (!string.IsNullOrEmpty(comSpec))
+                // Ensure the array won't be empty and the first element won't be null or empty string.
+                scriptArguments = scriptArguments.Where(argument => !string.IsNullOrEmpty(argument)).ToArray();
+
+                if (scriptArguments.Length == 0)
                 {
-                    scriptArguments =
-                        new[] { comSpec, "/C", "\"" }
-                        .Concat(scriptArguments)
-                        .Concat(new[] { "\"" })
-                        .ToArray();
+                    continue;
+                }
+
+                if (!PlatformHelper.IsMono)
+                {
+                    // Forward-slash is used in script blocked only. Replace them with back-slash to correctly
+                    // locate the script. The directory separator is platform-specific. 
+                    scriptArguments[0] = scriptArguments[0].Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+
+                    // Command-lines on Windows are executed via "cmd /C" in order
+                    // to support batch files, &&, built-in commands like echo, etc.
+                    // ComSpec is Windows-specific, and contains the full path to cmd.exe
+                    var comSpec = Environment.GetEnvironmentVariable("ComSpec");
+                    if (!string.IsNullOrEmpty(comSpec))
+                    {
+                        scriptArguments =
+                            new[] { comSpec, "/C", "\"" }
+                            .Concat(scriptArguments)
+                            .Concat(new[] { "\"" })
+                            .ToArray();
+                    }
+                }
+                else
+                {
+                    var scriptCandiate = scriptArguments[0] + ".sh";
+                    if (File.Exists(scriptCandiate))
+                    {
+                        scriptArguments[0] = scriptCandiate;
+                        scriptArguments = new[] { "/bin/bash" }.Concat(scriptArguments).ToArray();
+                    }
                 }
 
                 var startInfo = new ProcessStartInfo
@@ -44,12 +69,12 @@ namespace Microsoft.Framework.PackageManager
                     FileName = scriptArguments.FirstOrDefault(),
                     Arguments = String.Join(" ", scriptArguments.Skip(1)),
                     WorkingDirectory = project.ProjectDirectory,
-    #if ASPNET50
-                    UseShellExecute = false,
-    #endif
+#if ASPNET50
+                    UseShellExecute = false
+#endif
                 };
-                var process = Process.Start(startInfo);
 
+                var process = Process.Start(startInfo);
                 process.WaitForExit();
             }
         }
