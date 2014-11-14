16ae3296a62422f437cb0ea3972ef1e2404220a4
Wei Wang <chengtian.wang@gmail.com>
2014-11-13 17:38:28 -0800

    "kpm restore" shows chains to failed dependencies

--- src/Microsoft.Framework.PackageManager/Restore/RestoreCommand.cs
+++ src/Microsoft.Framework.PackageManager/Restore/RestoreCommand.cs
@@ -9,6 +9,7 @@ using System.IO.Compression;
 using System.Linq;
 using System.Runtime.Versioning;
 using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Framework.PackageManager.Packing;
 using Microsoft.Framework.PackageManager.Restore.NuGet;
@@ -213,9 +214,18 @@ namespace Microsoft.Framework.PackageManager
 
             Reports.Information.WriteLine(string.Format("{0}, {1}ms elapsed", "Resolving complete".Green(), sw.ElapsedMilliseconds));
 
+            Func<GraphNode, string> getDependencyType = node =>
+            {
+                if (projectProviders.Any(x => x == node.Item.Match.Provider))
+                {
+                    return "Project";
+                }
+                return "Package";
+            };
+
             var installItems = new List<GraphItem>();
             var missingItems = new HashSet<Library>();
-            ForEach(graphs, node =>
+            ForEach(graphs, (node, path)=>
             {
                 if (node == null || node.Library == null)
                 {
@@ -228,6 +238,7 @@ namespace Microsoft.Framework.PackageManager
                          missingItems.Add(node.Library))
                     {
                         Reports.Error.WriteLine(string.Format("Unable to locate {0} >= {1}", node.Library.Name.Red().Bold(), node.Library.Version));
+                        ShowFailedDependencyChain(path, node, getDependencyType);
                         success = false;
                     }
                     return;
@@ -239,6 +250,7 @@ namespace Microsoft.Framework.PackageManager
                     {
                         Reports.Error.WriteLine("Unable to locate {0} >= {1}. Do you mean {2}?",
                             node.Library.Name.Red().Bold(), node.Library.Version, node.Item.Match.Library.Name.Bold());
+                        ShowFailedDependencyChain(path, node, getDependencyType);
                         success = false;
                     }
                     return;
@@ -265,6 +277,21 @@ namespace Microsoft.Framework.PackageManager
             }
 
             return success;
+        }
+
+        private void ShowFailedDependencyChain(Stack<GraphNode> path, GraphNode failedNode, Func<GraphNode, string> getDependencyType)
+        {
+            var sb = new StringBuilder();
+            foreach (var node in path.Reverse())
+            {
+                sb.AppendFormat("{0}.{1} ({2})",
+                    node.Item.Match.Library.Name,
+                    node.Item.Match.Library.Version,
+                    getDependencyType(node));
+                sb.Append(" -> ");
+            }
+            sb.AppendFormat("{0}.{1}".Red().Bold(), failedNode.Library.Name, failedNode.Library.Version);
+            Reports.Error.WriteLine("  " + sb.ToString());
         }
 
         private async Task<bool> RestoreFromGlobalJson(string rootDirectory, string packagesDirectory)
@@ -514,12 +541,20 @@ namespace Microsoft.Framework.PackageManager
             }
         }
 
-        void ForEach(IEnumerable<GraphNode> nodes, Action<GraphNode> callback)
+        void ForEach(IEnumerable<GraphNode> nodes, Action<GraphNode, Stack<GraphNode>> callback)
+        {
+            var path = new Stack<GraphNode>();
+            ForEachCore(nodes, path, callback);
+        }
+
+        void ForEachCore(IEnumerable<GraphNode> nodes, Stack<GraphNode> path, Action<GraphNode, Stack<GraphNode>> callback)
         {
             foreach (var node in nodes)
             {
-                callback(node);
-                ForEach(node.Dependencies, callback);
+                callback(node, path);
+                path.Push(node);
+                ForEachCore(node.Dependencies, path, callback);
+                path.Pop();
             }
         }
 
