172efb9de6955126862f44265880ef2c060d3878
Wei Wang <chengtian.wang@gmail.com>
2014-11-19 18:51:11 -0800

    Handle the case where arguments contain double quotes

--- scripts/kpm.cmd
+++ scripts/kpm.cmd
@@ -3,7 +3,8 @@ SETLOCAL
 SET ERRORLEVEL=
 
 SET ARGS=%*
-IF NOT "%ARGS%"=="" SET ARGS=%ARGS:/?="/?"%
+SET ARGS_NO_QUOTE=%ARGS:"=%
+IF NOT "%ARGS_NO_QUOTE%"=="" SET ARGS=%ARGS:/?="/?"%
 
 "%~dp0klr" --appbase "%CD%" %K_OPTIONS% --lib "%~dp0lib\Microsoft.Framework.PackageManager" Microsoft.Framework.PackageManager --tools-path "%~dp0lib\Microsoft.Framework.PackageManager" %ARGS%
 
