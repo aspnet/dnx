27c948968cf1a6c26a5a2f3a93229aa99a9971d4
Wei Wang <chengtian.wang@gmail.com>
2014-11-21 15:03:16 -0800

    Fix broken scripts

--- scripts/k-crossgen.cmd
+++ scripts/k-crossgen.cmd
@@ -5,7 +5,7 @@ SET ERRORLEVEL=
 SET "KLR_RUNTIME_PATH=%~dp0."
 SET "CROSSGEN_PATH=%~dp0crossgen.exe"
 
-klr --lib "%~dp0lib\Microsoft.Framework.Project" "Microsoft.Framework.Project" crossgen --exePath "%CROSSGEN_PATH%" --runtimePath "%KLR_RUNTIME_PATH%" %*
+%~dp0klr --lib "%~dp0lib\Microsoft.Framework.Project" "Microsoft.Framework.Project" crossgen --exePath "%CROSSGEN_PATH%" --runtimePath "%KLR_RUNTIME_PATH%" %*
 
 exit /b %ERRORLEVEL%
 ENDLOCAL
--- scripts/kpm.cmd
+++ scripts/kpm.cmd
@@ -2,7 +2,7 @@
 SETLOCAL
 SET ERRORLEVEL=
 
-klr --appbase %CD% %K_OPTIONS% --lib "%~dp0lib\Microsoft.Framework.PackageManager" Microsoft.Framework.PackageManager --tools-path "%~dp0lib\Microsoft.Framework.PackageManager" %*
+%~dp0klr --appbase %CD% %K_OPTIONS% --lib "%~dp0lib\Microsoft.Framework.PackageManager" Microsoft.Framework.PackageManager --tools-path "%~dp0lib\Microsoft.Framework.PackageManager" %*
 
 exit /b %ERRORLEVEL%
 ENDLOCAL
