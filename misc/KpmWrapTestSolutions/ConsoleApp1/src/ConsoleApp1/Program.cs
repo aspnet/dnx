ffa6a167deffc9918a2b13338102db64404143c5
Wei Wang <chengtian.wang@gmail.com>
2014-12-02 13:50:39 -0800

    Add a solution that we test "kpm wrap" against

---
+++ misc/KpmWrapTestSolutions/ConsoleApp1/src/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine(typeof(LibraryAlpha.Class1).Assembly.Location);
+            Console.WriteLine(typeof(LibraryBeta.PCL.Desktop.Class1).Assembly.Location);
+            Console.WriteLine(typeof(LibraryEpsilon.Class1).Assembly.Location);
+            Console.WriteLine(typeof(LibraryGamma.Class1).Assembly.Location);
+            Console.WriteLine(typeof(System.Data.Entity.Database).Assembly.Location);
+            Console.WriteLine(typeof(System.Data.Entity.SqlServer.SqlFunctions).Assembly.Location);
+            Console.WriteLine(typeof(LibraryDelta.Class1).Assembly.Location);
+        }
+    }
+}
