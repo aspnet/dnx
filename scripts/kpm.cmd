a904b0d084fdeac5630155eed6e311310e93ebbc
shhsu <shhsu@microsoft.com>
2015-01-02 10:47:48 -0800

    kpm --native is broken without Microsoft.Framework.Project

--- scripts/kpm.cmd
+++ scripts/kpm.cmd
@@ -2,7 +2,7 @@
 SETLOCAL
 SET ERRORLEVEL=
 
-"%~dp0dotnet" %DOTNET_OPTIONS% "%~dp0lib\Microsoft.Framework.PackageManager\Microsoft.Framework.PackageManager.dll" %*
+"%~dp0dotnet" %DOTNET_OPTIONS% --lib "%~dp0lib\Microsoft.Framework.Project" "%~dp0lib\Microsoft.Framework.PackageManager\Microsoft.Framework.PackageManager.dll" %*
 
 exit /b %ERRORLEVEL%
 ENDLOCAL
