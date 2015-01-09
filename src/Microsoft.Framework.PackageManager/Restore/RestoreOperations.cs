aspnet/dnx

commit 17de54dd2e1aac0f661f60eb1ac63ac5fda586bc

Update the priority of restore from remote sources
Break the remote sources into two groups. The group of the http based
sources won't be executed unless the group of the local sources can't produce
a match. Among the same group all the sources are still executed parallel.

src/Microsoft.Framework.PackageManager/Restore/RestoreOperations.cs
@@ -1,16 +1,13 @@
 // Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.Framework.Runtime;
-using NuGet;
-using NuGet.Common;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
-using System.Runtime.Versioning;
 using System.Threading.Tasks;
+using Microsoft.Framework.Runtime;
+using NuGet;
 
 namespace Microsoft.Framework.PackageManager
 {
@@ -205,35 +202,29 @@ namespace Microsoft.Framework.PackageManager
 
         private async Task<WalkProviderMatch> FindLibraryBySnapshot(RestoreContext context, Library library, IEnumerable<IWalkProvider> providers)
         {
-            List<Task<WalkProviderMatch>> tasks = new List<Task<WalkProviderMatch>>();
-            foreach (var provider in providers)
-            {
-                tasks.Add(provider.FindLibraryBySnapshot(library, context.FrameworkName));
-            }
-            var matches = await Task.WhenAll(tasks);
-            WalkProviderMatch bestMatch = null;
-            foreach (var match in matches)
-            {
-                if (VersionUtility.ShouldUseConsidering(
-                    current: (bestMatch == null || bestMatch.Library == null) ? null : bestMatch.Library.Version,
-                    considering: (match == null || match.Library == null) ? null : match.Library.Version,
-                    ideal: library.Version))
-                {
-                    bestMatch = match;
-                }
-            }
-            return bestMatch;
+            return await FindLibrary(library, providers.Where(p => !p.IsHttp), provider => provider.FindLibraryBySnapshot(library, context.FrameworkName)) ??
+                   await FindLibrary(library, providers.Where(p => p.IsHttp), provider => provider.FindLibraryBySnapshot(library, context.FrameworkName));
         }
 
         private async Task<WalkProviderMatch> FindLibraryByVersion(RestoreContext context, Library library, IEnumerable<IWalkProvider> providers)
         {
-            List<Task<WalkProviderMatch>> tasks = new List<Task<WalkProviderMatch>>();
+            return await FindLibrary(library, providers.Where(p => !p.IsHttp), provider => provider.FindLibraryByVersion(library, context.FrameworkName)) ??
+                   await FindLibrary(library, providers.Where(p => p.IsHttp), provider => provider.FindLibraryByVersion(library, context.FrameworkName));
+        }
+
+        private static async Task<WalkProviderMatch> FindLibrary(
+            Library library,
+            IEnumerable<IWalkProvider> providers,
+            Func<IWalkProvider, Task<WalkProviderMatch>> action)
+        {
+            var tasks = new List<Task<WalkProviderMatch>>();
             foreach (var provider in providers)
             {
-                tasks.Add(provider.FindLibraryByVersion(library, context.FrameworkName));
+                tasks.Add(action(provider));
             }
-            var matches = await Task.WhenAll(tasks);
+
             WalkProviderMatch bestMatch = null;
+            var matches = await Task.WhenAll(tasks);
             foreach (var match in matches)
             {
                 if (VersionUtility.ShouldUseConsidering(
@@ -244,8 +235,8 @@ namespace Microsoft.Framework.PackageManager
                     bestMatch = match;
                 }
             }
+
             return bestMatch;
         }
     }
-
 }

src/Microsoft.Framework.PackageManager/Restore/LocalWalkProvider.cs
@@ -1,16 +1,13 @@
 // Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.Framework.PackageManager.Restore.NuGet;
-using Microsoft.Framework.Runtime;
-using NuGet;
-using NuGet.Common;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Runtime.Versioning;
 using System.Threading.Tasks;
+using Microsoft.Framework.Runtime;
+using NuGet;
 
 namespace Microsoft.Framework.PackageManager
 {
@@ -28,6 +25,7 @@ namespace Microsoft.Framework.PackageManager
         Task<WalkProviderMatch> FindLibraryBySnapshot(Library library, FrameworkName targetFramework);
         Task<IEnumerable<LibraryDependency>> GetDependencies(WalkProviderMatch match, FrameworkName targetFramework);
         Task CopyToAsync(WalkProviderMatch match, Stream stream);
+        bool IsHttp { get; }
     }
 
     public class LocalWalkProvider : IWalkProvider
@@ -38,6 +36,8 @@ namespace Microsoft.Framework.PackageManager
         {
             _dependencyProvider = dependencyProvider;
         }
+
+        public bool IsHttp { get; private set; }
 
         public Task<WalkProviderMatch> FindLibraryByName(string name, FrameworkName targetFramework)
         {

src/Microsoft.Framework.PackageManager/Restore/RemoteWalkProvider.cs
@@ -16,9 +16,15 @@ namespace Microsoft.Framework.PackageManager
     {
         private IPackageFeed _source;
 
-        public RemoteWalkProvider(IPackageFeed source)
+        public RemoteWalkProvider(IPackageFeed source, bool isHttp)
         {
             _source = source;
+            IsHttp = isHttp;
+        }
+
+        public bool IsHttp
+        {
+            get; private set;
         }
 
         public Task<WalkProviderMatch> FindLibraryByName(string name, FrameworkName targetFramework)
@@ -73,7 +79,7 @@ namespace Microsoft.Framework.PackageManager
                     return dependencySet
                         .SelectMany(x => x.Dependencies)
                         .Select(x => new LibraryDependency(name: x.Id,
-                            version: x.VersionSpec != null ? x.VersionSpec.MinVersion : null ))
+                            version: x.VersionSpec != null ? x.VersionSpec.MinVersion : null))
                         .ToList();
                 }
             }

src/Microsoft.Framework.PackageManager/Restore/RestoreCommand.cs
@@ -454,7 +454,8 @@ namespace Microsoft.Framework.PackageManager
                         new RemoteWalkProvider(
                             PackageFolderFactory.CreatePackageFolderFromPath(
                                 source.Source,
-                                Reports.Quiet)));
+                                Reports.Quiet),
+                            isHttp: false));
                 }
                 else
                 {
@@ -466,7 +467,8 @@ namespace Microsoft.Framework.PackageManager
                                 source.Password,
                                 NoCache,
                                 Reports,
-                                ignoreFailure: IgnoreFailedSources)));
+                                ignoreFailure: IgnoreFailedSources),
+                            isHttp: true));
                 }
             }
         }

commit a35b6badb50d4c6cd5deaa11ab9e6ff46571c5a8

Check all sources when there's a snapshot version.
- Fixed regression caused by
17de54dd2e1aac0f661f60eb1ac63ac5fda586bc

src/Microsoft.Framework.PackageManager/Restore/RestoreOperations.cs
@@ -247,6 +247,12 @@ namespace Microsoft.Framework.PackageManager
 
         private async Task<WalkProviderMatch> FindLibraryByVersion(RestoreContext context, Library library, IEnumerable<IWalkProvider> providers)
         {
+            if (library.Version.IsSnapshot)
+            {
+                // Don't optimize the non http path for snapshot versions or we'll miss things
+                return await FindLibrary(library, providers, provider => provider.FindLibraryByVersion(library, context.FrameworkName));
+            }
+
             return await FindLibrary(library, providers.Where(p => !p.IsHttp), provider => provider.FindLibraryByVersion(library, context.FrameworkName)) ??
                    await FindLibrary(library, providers.Where(p => p.IsHttp), provider => provider.FindLibraryByVersion(library, context.FrameworkName));
         }