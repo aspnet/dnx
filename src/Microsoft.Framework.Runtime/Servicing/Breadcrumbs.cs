6ed312bf6c747f35804e10031710869a17212a54
Victor Hurdugaci <victor.hurdugaci@microsoft.com>
2015-01-16 10:17:03 -0800

    Folder name is singular

--- src/Microsoft.Framework.Runtime/Servicing/Breadcrumbs.cs
+++ src/Microsoft.Framework.Runtime/Servicing/Breadcrumbs.cs
@@ -53,7 +53,7 @@ namespace Microsoft.Framework.Runtime.Servicing
                 programDataFolder,
                 "Microsoft",
                 "KRE",
-                "BreadcrumbsStore");
+                "BreadcrumbStore");
 
             return breadcrumbsFolder;
         }
