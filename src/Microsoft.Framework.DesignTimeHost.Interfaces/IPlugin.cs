378a9042bcf1e17c21fae4548346f00794e0bb3b
NTaylorMullen <nimullen@microsoft.com>
2015-02-04 23:38:39 -0800

    Add IPlugin support for DTH.
    - Added two interfaces exposed by a new assembly Microsoft.Framework.DesignTimeHost.Interfaces that are meant to be used by consumers: IPlugin and IPluginMessageBroker. These two are the bread and butter to receiving and sending messages for a DTH plugin.
    - Added a brand new message type to the ApplicationContext "Plugin".
    - Added a PluginHandler which breaks down the PluginMessage into three separate messages: register, unregister and message. A plugin must be registered before any messages can be sent to it.
    - Added resources to represent common error cases.
    
    #499

---
+++ src/Microsoft.Framework.DesignTimeHost.Interfaces/IPlugin.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Framework.DesignTimeHost
+{
+    public interface IPlugin
+    {
+        void ProcessMessage(JObject data);
+    }
+}
---
+++ src/Microsoft.Framework.DesignTimeHost/PluginHandler.cs
@@ -0,0 +1,143 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Concurrent;
+using System.Reflection;
+using Microsoft.Framework.DesignTimeHost.Models.IncomingMessages;
+using Microsoft.Framework.Runtime;
+using Microsoft.Framework.Runtime.Common.DependencyInjection;
+
+namespace Microsoft.Framework.DesignTimeHost
+{
+    public class PluginHandler
+    {
+        private readonly Action<object> _sendMessageMethod;
+        private readonly IServiceProvider _hostServices;
+        private readonly ConcurrentDictionary<string, IPlugin> _plugins;
+
+        public PluginHandler(IServiceProvider hostServices, Action<object> sendMessageMethod)
+        {
+            _sendMessageMethod = sendMessageMethod;
+            _hostServices = hostServices;
+            _plugins = new ConcurrentDictionary<string, IPlugin>(StringComparer.Ordinal);
+        }
+
+        public void ProcessMessage(PluginMessage data, IAssemblyLoadContext assemblyLoadContext)
+        {
+            switch (data.MessageName)
+            {
+                case "RegisterPlugin":
+                    ProcessRegisterMessage(data, assemblyLoadContext);
+                    break;
+                case "UnregisterPlugin":
+                    ProcessUnregisterMessage(data);
+                    break;
+                case "PluginMessage":
+                    ProcessPluginMessage(data);
+                    break;
+            }
+        }
+
+        private void ProcessPluginMessage(PluginMessage data)
+        {
+            IPlugin plugin;
+            if (_plugins.TryGetValue(data.PluginId, out plugin))
+            {
+                plugin.ProcessMessage(data.Data);
+            }
+            else
+            {
+                throw new InvalidOperationException(
+                    Resources.FormatPlugin_UnregisteredPluginIdCannotReceiveMessages(data.PluginId));
+            }
+        }
+
+        private void ProcessUnregisterMessage(PluginMessage data)
+        {
+            IPlugin removedPlugin;
+            if (!_plugins.TryRemove(data.PluginId, out removedPlugin))
+            {
+                throw new InvalidOperationException(
+                    Resources.FormatPlugin_UnregisteredPluginIdCannotUnregister(data.PluginId));
+            }
+        }
+
+        private void ProcessRegisterMessage(PluginMessage data, IAssemblyLoadContext assemblyLoadContext)
+        {
+            var pluginId = data.PluginId;
+            var registerData = data.Data.ToObject<PluginRegisterData>();
+            var assembly = assemblyLoadContext.Load(registerData.AssemblyName);
+            var pluginType = assembly.GetType(registerData.TypeName);
+
+            if (pluginType != null)
+            {
+                // We build out a custom plugin service provider to add a PluginMessageBroker to the potential
+                // services that can be used to construct an IPlugin.
+                var pluginServiceProvider = new PluginServiceProvider(
+                    _hostServices,
+                    assemblyLoadContext,
+                    messageBroker: new PluginMessageBroker(pluginId, _sendMessageMethod));
+
+                var plugin = ActivatorUtilities.CreateInstance(pluginServiceProvider, pluginType) as IPlugin;
+
+                if (plugin == null)
+                {
+                    throw new InvalidOperationException(
+                        Resources.FormatPlugin_CannotProcessMessageInvalidPluginType(
+                            pluginId,
+                            pluginType.FullName,
+                            typeof(IPlugin).FullName));
+                }
+
+                _plugins[pluginId] = plugin;
+            }
+        }
+
+        private class PluginServiceProvider : IServiceProvider
+        {
+            private static readonly TypeInfo MessageBrokerTypeInfo = typeof(IPluginMessageBroker).GetTypeInfo();
+            private static readonly TypeInfo AssemblyLoadContextTypeInfo = typeof(IAssemblyLoadContext).GetTypeInfo();
+            private readonly IServiceProvider _hostServices;
+            private readonly IAssemblyLoadContext _assemblyLoadContext;
+            private readonly PluginMessageBroker _messageBroker;
+
+            public PluginServiceProvider(
+                IServiceProvider hostServices,
+                IAssemblyLoadContext assemblyLoadContext,
+                PluginMessageBroker messageBroker)
+            {
+                _hostServices = hostServices;
+                _assemblyLoadContext = assemblyLoadContext;
+                _messageBroker = messageBroker;
+            }
+
+            public object GetService(Type serviceType)
+            {
+                var hostProvidedService = _hostServices.GetService(serviceType);
+
+                if (hostProvidedService == null)
+                {
+                    var serviceTypeInfo = serviceType.GetTypeInfo();
+
+                    if (MessageBrokerTypeInfo.IsAssignableFrom(serviceTypeInfo))
+                    {
+                        return _messageBroker;
+                    }
+                    else if (AssemblyLoadContextTypeInfo.IsAssignableFrom(serviceTypeInfo))
+                    {
+                        return _assemblyLoadContext;
+                    }
+                }
+
+                return hostProvidedService;
+            }
+        }
+
+        private class PluginRegisterData
+        {
+            public string AssemblyName { get; set; }
+            public string TypeName { get; set; }
+        }
+    }
+}
