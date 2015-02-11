efcaf801cd399f70772946a1e87eccbb8092f78f
Wei Wang <chengtian.wang@gmail.com>
2015-02-11 10:34:58 -0800

    Follow up: still being blocked by TPA list

--- src/Microsoft.Framework.PackageManager/Bundle/BundleRuntime.cs
+++ src/Microsoft.Framework.PackageManager/Bundle/BundleRuntime.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -36,41 +37,13 @@ namespace Microsoft.Framework.PackageManager.Bundle
 
             if (root.OneFolder)
             {
-                var assemblyCopyFlag = new Dictionary<string, bool>();
-                foreach (var assembly in Directory.EnumerateFiles(_runtimeBinPath, "*.dll"))
-                {
-                    assemblyCopyFlag.Add(Path.GetFileNameWithoutExtension(assembly), false);
-                }
-
-                var runtimeBasicManagedAssemblyNames = new List<string> { Runtime.Constants.BootstrapperHostName };
-
-                if (VersionUtility.IsDesktop(Framework))
-                {
-                    runtimeBasicManagedAssemblyNames.Add(Runtime.Constants.BootstrapperClrManagedName);
-                }
-                else
-                {
-                    runtimeBasicManagedAssemblyNames.Add(Runtime.Constants.BootstrapperCoreclrManagedName);
-                }
-
-                var runtimeBasicManagedAssemblyPaths = runtimeBasicManagedAssemblyNames
-                    .Select(x => Path.Combine(_runtimeBinPath, x + ".dll"));
-
-                var bundledAppAssemblyPaths = Directory.EnumerateFiles(root.OutputPath, "*.dll");
-
-                var accessor = root.HostServices.GetService(typeof(IAssemblyLoadContextAccessor)) as IAssemblyLoadContextAccessor;
-                var assemblyLoadContext = accessor.Default;
-                foreach (var assemblyPath in runtimeBasicManagedAssemblyPaths.Concat(bundledAppAssemblyPaths))
-                {
-                    MarkAssembliesToCopy(assemblyPath, assemblyCopyFlag, assemblyLoadContext);
-                }
-
+                var assemblyNames = GetMinimalRuntimeAssemblyClosure(root);
                 var extensions = new[] { ".dll", ".pdb", ".xml" };
-                foreach (var assemblyName in assemblyCopyFlag.Where(x => x.Value).Select(x => x.Key))
+                foreach (var name in assemblyNames)
                 {
                     foreach (var extension in extensions)
                     {
-                        var fileName = assemblyName + extension;
+                        var fileName = name + extension;
                         var path = Path.Combine(_runtimeBinPath, fileName);
                         var dest = Path.Combine(root.OutputPath, fileName);
 
@@ -82,6 +55,19 @@ namespace Microsoft.Framework.PackageManager.Bundle
                             File.Copy(path, dest);
                         }
                     }
+                }
+
+                var extraFilesToCopy = new List<string> { Runtime.Constants.BootstrapperExeName + ".exe" };
+                if (VersionUtility.IsDesktop(Framework))
+                {
+                    extraFilesToCopy.Add(Runtime.Constants.BootstrapperClrName + ".config");
+                }
+
+                foreach (var fileName in extraFilesToCopy)
+                {
+                    var path = Path.Combine(_runtimeBinPath, fileName);
+                    var dest = Path.Combine(root.OutputPath, fileName);
+                    File.Copy(path, dest, overwrite: true);
                 }
 
                 return;
@@ -108,6 +94,44 @@ namespace Microsoft.Framework.PackageManager.Bundle
             }
         }
 
+        private IEnumerable<string> GetMinimalRuntimeAssemblyClosure(BundleRoot root)
+        {
+            var assemblyCopyFlag = new Dictionary<string, bool>();
+            foreach (var assembly in Directory.EnumerateFiles(_runtimeBinPath, "*.dll"))
+            {
+                assemblyCopyFlag.Add(Path.GetFileNameWithoutExtension(assembly), false);
+            }
+
+            var runtimeBasicManagedAssemblyNames = new List<string> { Runtime.Constants.BootstrapperHostName };
+            var runtimeBasicNativeAssemblyNames = new List<string>();
+
+            if (VersionUtility.IsDesktop(Framework))
+            {
+                runtimeBasicManagedAssemblyNames.Add(Runtime.Constants.BootstrapperClrManagedName);
+                runtimeBasicNativeAssemblyNames.Add(Runtime.Constants.BootstrapperClrName);
+            }
+            else
+            {
+                runtimeBasicManagedAssemblyNames.Add(Runtime.Constants.BootstrapperCoreclrManagedName);
+                runtimeBasicNativeAssemblyNames.Add(Runtime.Constants.BootstrapperCoreclrName);
+                runtimeBasicNativeAssemblyNames.Add("coreclr");
+            }
+
+            var runtimeBasicManagedAssemblyPaths = runtimeBasicManagedAssemblyNames
+                .Select(x => Path.Combine(_runtimeBinPath, x + ".dll"));
+
+            var bundledAppAssemblyPaths = Directory.EnumerateFiles(root.OutputPath, "*.dll");
+
+            var accessor = root.HostServices.GetService(typeof(IAssemblyLoadContextAccessor)) as IAssemblyLoadContextAccessor;
+            var assemblyLoadContext = accessor.Default;
+            foreach (var assemblyPath in runtimeBasicManagedAssemblyPaths.Concat(bundledAppAssemblyPaths))
+            {
+                MarkAssembliesToCopy(assemblyPath, assemblyCopyFlag, assemblyLoadContext);
+            }
+
+            return assemblyCopyFlag.Where(x => x.Value).Select(x => x.Key).Concat(runtimeBasicNativeAssemblyNames);
+        }
+
         private void MarkAssembliesToCopy(string rootAssemblyPath, Dictionary<string, bool> assemblyCopyFlag,
             IAssemblyLoadContext assemblyLoadContext)
         {
@@ -118,13 +142,15 @@ namespace Microsoft.Framework.PackageManager.Bundle
                 assemblyCopyFlag[key] = true;
             }
 #else
+            var rootAssemblyName = Path.GetFileNameWithoutExtension(rootAssemblyPath);
+            assemblyCopyFlag[rootAssemblyName] = true;
+
             var rootAssembly = assemblyLoadContext.LoadFile(rootAssemblyPath);
             foreach (var reference in rootAssembly.GetReferencedAssemblies())
             {
                 bool marked;
                 if (assemblyCopyFlag.TryGetValue(reference.Name, out marked) && !marked)
                 {
-                    assemblyCopyFlag[reference.Name] = true;
                     MarkAssembliesToCopy(
                         Path.Combine(_runtimeBinPath, reference.Name + ".dll"),
                         assemblyCopyFlag,
