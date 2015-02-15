bd63f43a495d0ba43bb8d1a3f17e038e70513ec0
Troy Dai <troy.dai@outlook.com>
2015-02-14 20:11:33 -0800

    Check source folder and files before copy

--- build/batchcopy.ps1
+++ build/batchcopy.ps1
@@ -4,5 +4,10 @@ $src = $src -replace "/", "\"
 $dst = $dst -replace "/", "\"
 
 Write-Host "run: xcopy /F /Y /I $src $dst"
-xcopy /F /Y /I $src $dst
+
+if ((Test-Path $src) -and (ls $src))
+{
+    xcopy /F /Y /I $src $dst
+}
+
 exit $LASTEXITCODE
