c569f0ff57c45b9b6dc23195b8b80bcacfdd84ba
Victor Hurdugaci <victor.hurdugaci@microsoft.com>
2015-02-17 12:47:22 -0800

    Servicing index should always use the latest entry for a particular library. Includes some tests for servicing too.

--- src/Microsoft.Framework.Runtime/Servicing/ServicingIndex.cs
+++ src/Microsoft.Framework.Runtime/Servicing/ServicingIndex.cs
@@ -10,13 +10,13 @@ namespace Microsoft.Framework.Runtime.Servicing
 {
     public class ServicingIndex
     {
-        string _folderPath;
-        Dictionary<EntryKey, Entry> _entries = new Dictionary<EntryKey, Entry>();
+        private string _patchesRootFolder;
+        private Dictionary<EntryKey, Entry> _entries = new Dictionary<EntryKey, Entry>();
 
-        public void Initialize(string folderPath)
+        public void Initialize(string servicingFolder)
         {
-            _folderPath = folderPath;
-            var indexFilePath = Path.Combine(folderPath, "index.txt");
+            _patchesRootFolder = servicingFolder;
+            var indexFilePath = Path.Combine(servicingFolder, "index.txt");
             if (!File.Exists(indexFilePath))
             {
                 Logger.TraceInformation("[{0}]: Servicing index not found at {1}", GetType().Name, indexFilePath);
@@ -28,54 +28,61 @@ namespace Microsoft.Framework.Runtime.Servicing
             }
             using (var stream = new FileStream(indexFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
             {
-                using (var reader = new StreamReader(stream))
-                {
-                    var lineNumber = 0;
-                    for (var line = reader.ReadLine(); line != null; line = reader.ReadLine())
-                    {
-                        lineNumber += 1;
+                Initialize(servicingFolder, stream);
+            }
+        }
 
-                        if (line.StartsWith("#") || string.IsNullOrWhiteSpace(line))
+        internal void Initialize(string patchesFolder, Stream indexStream)
+        {
+            _patchesRootFolder = patchesFolder;
+
+            using (var reader = new StreamReader(indexStream))
+            {
+                var lineNumber = 0;
+                for (var line = reader.ReadLine(); line != null; line = reader.ReadLine())
+                {
+                    lineNumber += 1;
+
+                    if (line.StartsWith("#") || string.IsNullOrWhiteSpace(line))
+                    {
+                        continue;
+                    }
+                    var parts = line.Split(new[] { '=' }, 2);
+                    if (parts.Length != 2)
+                    {
+                        Logger.TraceInformation("[{0}]. Line {1}: malformed servicing file", GetType().Name, lineNumber);
+                        continue;
+                    }
+                    parts[0] = parts[0].Trim();
+                    parts[1] = parts[1].Trim();
+                    if (parts[0].Length == 0 || parts[1].Length == 0)
+                    {
+                        Logger.TraceInformation("[{0}]. Line {1}: malformed servicing file", GetType().Name, lineNumber);
+                        continue;
+                    }
+                    var fields = parts[0].Split(new[] { '|' });
+                    if (String.Equals(fields[0], "nupkg", StringComparison.Ordinal))
+                    {
+                        if (fields.Length != 4)
                         {
+                            Logger.TraceInformation("[{0}]. Line {1}: malformed servicing key", GetType().Name, lineNumber);
                             continue;
                         }
-                        var parts = line.Split(new[] { '=' }, 2);
-                        if (parts.Length != 2)
+                        SemanticVersion version;
+                        if (!SemanticVersion.TryParseStrict(fields[2], out version))
                         {
-                            Logger.TraceInformation("[{0}]: {1}({2}): malformed servicing file", GetType().Name, indexFilePath, lineNumber);
+                            Logger.TraceInformation("[{0}]. Line {1}: malformed servicing version ", GetType().Name, lineNumber);
                             continue;
                         }
-                        parts[0] = parts[0].Trim();
-                        parts[1] = parts[1].Trim();
-                        if (parts[0].Length == 0 || parts[1].Length == 0)
+                        var key = new EntryKey(fields[1], version);
+                        Entry entry;
+                        if (!_entries.TryGetValue(key, out entry))
                         {
-                            Logger.TraceInformation("[{0}]: {1}({2}): malformed servicing file", GetType().Name, indexFilePath, lineNumber);
-                            continue;
+                            Logger.TraceInformation("[{0}]: Adding entry for {1} {2}", GetType().Name, key.Id, key.Version);
+                            entry = new Entry();
+                            _entries.Add(key, entry);
                         }
-                        var fields = parts[0].Split(new[] { '|' });
-                        if (String.Equals(fields[0], "nupkg", StringComparison.Ordinal))
-                        {
-                            if (fields.Length != 4)
-                            {
-                                Logger.TraceInformation("[{0}]: {1}({2}): malformed servicing key", GetType().Name, indexFilePath, lineNumber);
-                                continue;
-                            }
-                            SemanticVersion version;
-                            if (!SemanticVersion.TryParseStrict(fields[2], out version))
-                            {
-                                Logger.TraceInformation("[{0}]: {1}({2}): malformed servicing version ", GetType().Name, indexFilePath, lineNumber);
-                                continue;
-                            }
-                            var key = new EntryKey(fields[1], version);
-                            Entry entry;
-                            if (!_entries.TryGetValue(key, out entry))
-                            {
-                                Logger.TraceInformation("[{0}]: Adding entry for {1} {2}", GetType().Name, key.Id, key.Version);
-                                entry = new Entry();
-                                _entries.Add(key, entry);
-                            }
-                            entry.Mappings.Add(new EntryMapping(fields[3], parts[1]));
-                        }
+                        entry.Mappings.Add(new EntryMapping(fields[3], parts[1]));
                     }
                 }
             }
@@ -87,11 +94,14 @@ namespace Microsoft.Framework.Runtime.Servicing
             if (_entries.TryGetValue(new EntryKey(packageId, packageVersion), out entry))
             {
                 var normalizedAssetPath = assetPath.Replace('\\', '/');
-                foreach (var mapping in entry.Mappings)
+
+                // Use the last entry from the index file
+                for (int i = entry.Mappings.Count - 1; i >= 0; i--)
                 {
+                    var mapping = entry.Mappings[i];
                     if (string.Equals(normalizedAssetPath, mapping.AssetPath, StringComparison.OrdinalIgnoreCase))
                     {
-                        replacementPath = Path.Combine(_folderPath, mapping.ReplacementPath);
+                        replacementPath = Path.Combine(_patchesRootFolder, mapping.ReplacementPath);
                         Logger.TraceInformation("[{0}]: Using replacement {4} for {1} {2} {3}", GetType().Name, packageId, packageVersion, assetPath, replacementPath);
                         return true;
                     }
