ee36cfe9d00338da353db18b65dcc50fdf313d38
N. Taylor Mullen <nimullen@microsoft.com>
2015-02-16 17:31:27 -0800

    Add tests to validate PluginHandler and PluginMessageBroker.
    - Added several utility test classes to enable easy test scenarios for the DTH: TestAssembly, TestAssemblyLoadContext, TestServiceProvider.
    
    #499

---
+++ test/Microsoft.Framework.DesignTimeHost.Tests/PluginMessageBrokerFacts.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Xunit;
+
+namespace Microsoft.Framework.DesignTimeHost
+{
+    public class PluginMessageBrokerFacts
+    {
+        [Fact]
+        public void SendMessage_CallsIntoSendMessageMethod()
+        {
+            var called = false;
+            var pluginMessageBroker = 
+                new PluginMessageBroker(pluginId: Guid.NewGuid().ToString(), sendMessageMethod: (_) => called = true);
+
+            pluginMessageBroker.SendMessage(string.Empty);
+
+            Assert.True(called);
+        }
+
+        [Fact]
+        public void SendMessage_WrapsData()
+        {
+            PluginMessageBroker.PluginMessageWrapperData calledWith = null;
+            var expectedPluginId = "d81b8ad8-306d-474b-b8a9-b25c7f80be7e"; // Random, hardcoded GUID.
+            var pluginMessageBroker = new PluginMessageBroker(
+                expectedPluginId,
+                sendMessageMethod: (data) => calledWith = (PluginMessageBroker.PluginMessageWrapperData)data);
+
+            pluginMessageBroker.SendMessage("Hello World");
+
+            Assert.NotNull(calledWith);
+            Assert.Equal(expectedPluginId, calledWith.PluginId);
+            Assert.Equal("Hello World", (string)calledWith.Data, StringComparer.Ordinal);
+        }
+    }
+}
---
+++ test/Microsoft.Framework.DesignTimeHost.Tests/TestAssemblyLoadContext.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Microsoft.Framework.Runtime;
+
+namespace Microsoft.Framework.DesignTimeHost
+{
+    public class TestAssemblyLoadContext : IAssemblyLoadContext
+    {
+        private readonly IReadOnlyDictionary<string, Assembly> _assemblyNameLookups;
+
+        public TestAssemblyLoadContext(IReadOnlyDictionary<string, Assembly> assemblyNameLookups)
+        {
+            _assemblyNameLookups = assemblyNameLookups;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Assembly Load(string name)
+        {
+            return _assemblyNameLookups[name];
+        }
+
+        public Assembly LoadFile(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Assembly LoadStream(Stream assemblyStream, Stream assemblySymbols)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
