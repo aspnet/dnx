b6a74a20aab18a72a844f6ba3a99c65643b308fa
Wei Wang <chengtian.wang@gmail.com>
2015-02-17 22:36:26 -0800

    Internalize FileSystemGlobbing

---
+++ ext/compiler/preprocess/Internalization.cs
@@ -0,0 +1,101 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Framework.Runtime;
+using Microsoft.Framework.Runtime.Roslyn;
+
+namespace Runtime.Ext.Compiler.Preprocess
+{
+    public class Internalization : ICompileModule
+    {
+        private readonly string _appBase;
+
+        public Internalization(IServiceProvider services)
+        {
+            var appEnv = services.GetService(typeof(IApplicationEnvironment)) as IApplicationEnvironment;
+            _appBase = appEnv.ApplicationBasePath;
+        }
+
+        public void BeforeCompile(IBeforeCompileContext context)
+        {
+            var submodulesDir = Path.Combine(_appBase, "..", "..", "submodules");
+            var replacementDict = new Dictionary<SyntaxTree, SyntaxTree>();
+            var removeList = new List<SyntaxTree>();
+
+            foreach (var tree in context.Compilation.SyntaxTrees)
+            {
+                if (string.IsNullOrEmpty(tree.FilePath) ||
+                    !IsChildOfDirectory(dir: submodulesDir, candidate: tree.FilePath))
+                {
+                    continue;
+                }
+                
+                if (string.Equals("AssemblyInfo.cs", Path.GetFileName(tree.FilePath),
+                    StringComparison.OrdinalIgnoreCase))
+                {
+                    removeList.Add(tree);
+                    continue;
+                }
+
+                var root = tree.GetRoot();
+
+                var targetSyntaxKinds = new[] {
+                    SyntaxKind.ClassDeclaration,
+                    SyntaxKind.InterfaceDeclaration,
+                    SyntaxKind.StructDeclaration,
+                    SyntaxKind.EnumDeclaration
+                };
+
+                var typeDeclarations = root.DescendantNodes()
+                    .Where(x => targetSyntaxKinds.Contains(x.Kind()))
+                    .OfType<BaseTypeDeclarationSyntax>();
+                var publicKeywordTokens = new List<SyntaxToken>();
+
+                foreach (var declaration in typeDeclarations)
+                {
+                    var publicKeywordToken = declaration.Modifiers
+                        .SingleOrDefault(x => x.Kind() == SyntaxKind.PublicKeyword);
+                    if (publicKeywordToken != default(SyntaxToken))
+                    {
+                        publicKeywordTokens.Add(publicKeywordToken);
+                    }
+                }
+
+                if (publicKeywordTokens.Any())
+                {
+                    root = root.ReplaceTokens(publicKeywordTokens,
+                        (_, oldToken) => SyntaxFactory.ParseToken("internal").WithTriviaFrom(oldToken));
+                }
+
+                replacementDict.Add(tree,
+                    SyntaxFactory.SyntaxTree(root, tree.Options, tree.FilePath, tree.GetText().Encoding));
+            }
+
+            context.Compilation = context.Compilation.RemoveSyntaxTrees(removeList);
+            foreach (var pair in replacementDict)
+            {
+                context.Compilation = context.Compilation.ReplaceSyntaxTree(pair.Key, pair.Value);
+            }
+        }
+
+        public void AfterCompile(IAfterCompileContext context)
+        {
+
+        }
+
+        private static bool IsChildOfDirectory(string dir, string candidate)
+        {
+            dir = Path.GetFullPath(dir);
+            dir = dir.EndsWith(Path.DirectorySeparatorChar.ToString()) ? dir : dir + Path.DirectorySeparatorChar;
+            candidate = Path.GetFullPath(candidate);
+            return candidate.StartsWith(dir, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
