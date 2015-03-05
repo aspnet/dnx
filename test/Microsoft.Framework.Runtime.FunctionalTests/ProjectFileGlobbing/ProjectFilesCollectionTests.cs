4d5773893ed63837a07fd0d182a82d6467a9f859
anurse <andrew@andrewnurse.net>
2015-03-05 14:12:58 -0800

    fix build

--- test/Microsoft.Framework.Runtime.FunctionalTests/ProjectFileGlobbing/ProjectFilesCollectionTests.cs
+++ test/Microsoft.Framework.Runtime.FunctionalTests/ProjectFileGlobbing/ProjectFilesCollectionTests.cs
@@ -4,7 +4,6 @@
 using System.Linq;
 using System.IO;
 using Microsoft.Framework.Runtime.FunctionalTests.Utilities;
-using Microsoft.Framework.Runtime.Hosting;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Xunit;
--- test/Microsoft.Framework.Runtime.FunctionalTests/ProjectFileGlobbing/ProjectFilesTests.cs
+++ test/Microsoft.Framework.Runtime.FunctionalTests/ProjectFileGlobbing/ProjectFilesTests.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.IO;
-using Microsoft.Framework.Runtime.Hosting;
 
 namespace Microsoft.Framework.Runtime.FunctionalTests.ProjectFileGlobbing
 {
