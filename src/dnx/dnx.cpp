0c6fe84567a78b1ea37a872cb76153cedc6a77cc
Matt Ellis <matell@microsoft.com>
2015-03-05 18:59:07 -0800

    Use TCHAR in native bootstrapper
    We wish to share the source for the native bootstrapper between Windows
    and *NIX.  The first step is doing so is to move away from WCHAR, since
    it is not portable across platforms.  Instead, we'll use TCHAR, which
    we'll define to just be char on *NIX platforms.  This forces developers
    in klr to not assume that a character is two bytes.
    
    At runtime, there's no change at runtime. With _UNICODE defined, TCHAR
    is alias for WCHAR, LPCTSTR is an alias for LPCWSTR, etc.

--- src/dnx/dnx.cpp
+++ src/dnx/dnx.cpp
@@ -3,16 +3,16 @@
 #include "stdafx.h"
 #include "dnx.h"
 
-void GetModuleDirectory(HMODULE module, LPWSTR szPath)
+void GetModuleDirectory(HMODULE module, LPTSTR szPath)
 {
     DWORD dirLength = GetModuleFileName(module, szPath, MAX_PATH);
-    for (dirLength--; dirLength >= 0 && szPath[dirLength] != '\\'; dirLength--);
-    szPath[dirLength + 1] = L'\0';
+    for (dirLength--; dirLength >= 0 && szPath[dirLength] != _T('\\'); dirLength--);
+    szPath[dirLength + 1] = _T('\0');
 }
 
-int LastIndexOfChar(LPCWSTR const pszStr, WCHAR c)
+int LastIndexOfChar(LPCTSTR const pszStr, TCHAR c)
 {
-    int nIndex = wcsnlen_s(pszStr, MAX_PATH) - 1;
+    int nIndex = _tcsnlen(pszStr, MAX_PATH) - 1;
     do
     {
         if (pszStr[nIndex] == c)
@@ -24,15 +24,15 @@ int LastIndexOfChar(LPCWSTR const pszStr, WCHAR c)
     return nIndex;
 }
 
-bool StringsEqual(LPCWSTR const pszStrA, LPCWSTR const pszStrB)
+bool StringsEqual(LPCTSTR const pszStrA, LPCTSTR const pszStrB)
 {
-    return ::_wcsnicmp(pszStrA, pszStrB, MAX_PATH) == 0;
+    return ::_tcsnicmp(pszStrA, pszStrB, MAX_PATH) == 0;
 }
 
-bool StringEndsWith(LPCWSTR const pszStr, LPCWSTR const pszSuffix)
+bool StringEndsWith(LPCTSTR const pszStr, LPCTSTR const pszSuffix)
 {
-    int nStrLen = wcsnlen_s(pszStr, MAX_PATH);
-    int nSuffixLen = wcsnlen_s(pszSuffix, MAX_PATH);
+    int nStrLen = _tcsnlen(pszStr, MAX_PATH);
+    int nSuffixLen = _tcsnlen(pszSuffix, MAX_PATH);
     int nOffset = nStrLen - nSuffixLen;
 
     if (nOffset < 0)
@@ -43,55 +43,55 @@ bool StringEndsWith(LPCWSTR const pszStr, LPCWSTR const pszSuffix)
     return StringsEqual(pszStr + nOffset, pszSuffix);
 }
 
-int LastPathSeparatorIndex(LPCWSTR const pszPath)
+int LastPathSeparatorIndex(LPCTSTR const pszPath)
 {
-    int nLastSlashIndex = LastIndexOfChar(pszPath, L'/');
-    int nLastBackSlashIndex = LastIndexOfChar(pszPath, L'\\');
+    int nLastSlashIndex = LastIndexOfChar(pszPath, _T('/'));
+    int nLastBackSlashIndex = LastIndexOfChar(pszPath, _T('\\'));
     return max(nLastSlashIndex, nLastBackSlashIndex);
 }
 
-void GetParentDir(LPCWSTR const pszPath, LPWSTR const pszParentDir)
+void GetParentDir(LPCTSTR const pszPath, LPTSTR const pszParentDir)
 {
     int nLastSeparatorIndex = LastPathSeparatorIndex(pszPath);
     if (nLastSeparatorIndex < 0)
     {
-        StringCchCopyW(pszParentDir, MAX_PATH, L".");
+        _tcscpy_s(pszParentDir, MAX_PATH, _T("."));
         return;
     }
 
-    CopyMemory(pszParentDir, pszPath, (nLastSeparatorIndex + 1) * sizeof(WCHAR));
-    pszParentDir[nLastSeparatorIndex + 1] = L'\0';
+    CopyMemory(pszParentDir, pszPath, (nLastSeparatorIndex + 1) * sizeof(TCHAR));
+    pszParentDir[nLastSeparatorIndex + 1] = _T('\0');
 }
 
-void GetFileName(LPCWSTR const pszPath, LPWSTR const pszFileName)
+void GetFileName(LPCTSTR const pszPath, LPTSTR const pszFileName)
 {
     int nLastSeparatorIndex = LastPathSeparatorIndex(pszPath);
 
     if (nLastSeparatorIndex < 0)
     {
-        StringCchCopyW(pszFileName, MAX_PATH, pszPath);
+        _tcscpy_s(pszFileName, MAX_PATH, pszPath);
         return;
     }
 
-    StringCchCopyW(pszFileName, MAX_PATH, pszPath + nLastSeparatorIndex + 1);
+    _tcscpy_s(pszFileName, MAX_PATH, pszPath + nLastSeparatorIndex + 1);
 }
 
-int BootstrapperOptionValueNum(LPCWSTR pszCandidate)
+int BootstrapperOptionValueNum(LPCTSTR pszCandidate)
 {
-    if (StringsEqual(pszCandidate, L"--appbase") ||
-        StringsEqual(pszCandidate, L"--lib") ||
-        StringsEqual(pszCandidate, L"--packages") ||
-        StringsEqual(pszCandidate, L"--configuration") ||
-        StringsEqual(pszCandidate, L"--port"))
+    if (StringsEqual(pszCandidate, _T("--appbase")) ||
+        StringsEqual(pszCandidate, _T("--lib")) ||
+        StringsEqual(pszCandidate, _T("--packages")) ||
+        StringsEqual(pszCandidate, _T("--configuration")) ||
+        StringsEqual(pszCandidate, _T("--port")))
     {
         return 1;
     }
-    else if (StringsEqual(pszCandidate, L"--watch") ||
-        StringsEqual(pszCandidate, L"--debug") ||
-        StringsEqual(pszCandidate, L"--help") ||
-        StringsEqual(pszCandidate, L"-h") ||
-        StringsEqual(pszCandidate, L"-?") ||
-        StringsEqual(pszCandidate, L"--version"))
+    else if (StringsEqual(pszCandidate, _T("--watch")) ||
+        StringsEqual(pszCandidate, _T("--debug")) ||
+        StringsEqual(pszCandidate, _T("--help")) ||
+        StringsEqual(pszCandidate, _T("-h")) ||
+        StringsEqual(pszCandidate, _T("-?")) ||
+        StringsEqual(pszCandidate, _T("--version")))
     {
         return 0;
     }
@@ -100,7 +100,7 @@ int BootstrapperOptionValueNum(LPCWSTR pszCandidate)
     return -1;
 }
 
-void FreeExpandedCommandLineArguments(int nArgc, LPWSTR* ppszArgv)
+void FreeExpandedCommandLineArguments(int nArgc, LPTSTR* ppszArgv)
 {
     for (int i = 0; i < nArgc; ++i)
     {
@@ -109,7 +109,7 @@ void FreeExpandedCommandLineArguments(int nArgc, LPWSTR* ppszArgv)
     delete[] ppszArgv;
 }
 
-bool ExpandCommandLineArguments(int nArgc, LPWSTR* ppszArgv, int& nExpandedArgc, LPWSTR*& ppszExpandedArgv)
+bool ExpandCommandLineArguments(int nArgc, LPTSTR* ppszArgv, int& nExpandedArgc, LPTSTR*& ppszExpandedArgv)
 {
     if (nArgc == 0)
     {
@@ -119,16 +119,16 @@ bool ExpandCommandLineArguments(int nArgc, LPWSTR* ppszArgv, int& nExpandedArgc,
     for (int i = 0; i < nArgc; ++i)
     {
         // If '--appbase' is already given and it has a value
-        if (StringsEqual(ppszArgv[i], L"--appbase") && (i < nArgc - 1))
+        if (StringsEqual(ppszArgv[i], _T("--appbase")) && (i < nArgc - 1))
         {
             return false;
         }
     }
 
     nExpandedArgc = nArgc + 2;
-    ppszExpandedArgv = new LPWSTR[nExpandedArgc];
-    memset(ppszExpandedArgv, 0, nExpandedArgc*sizeof(LPWSTR));
-    WCHAR szParentDir[MAX_PATH];
+    ppszExpandedArgv = new LPTSTR[nExpandedArgc];
+    memset(ppszExpandedArgv, 0, nExpandedArgc*sizeof(LPTSTR));
+    TCHAR szParentDir[MAX_PATH];
 
     // Copy all arguments (options & values) as is before the project.json/assembly path
     int nPathArgIndex = -1;
@@ -144,14 +144,14 @@ bool ExpandCommandLineArguments(int nArgc, LPWSTR* ppszArgv, int& nExpandedArgc,
         }
 
         // Copy the option
-        ppszExpandedArgv[nPathArgIndex] = new WCHAR[MAX_PATH];
-        StringCchCopyW(ppszExpandedArgv[nPathArgIndex], MAX_PATH, ppszArgv[nPathArgIndex]);
+        ppszExpandedArgv[nPathArgIndex] = new TCHAR[MAX_PATH];
+        _tcscpy_s(ppszExpandedArgv[nPathArgIndex], MAX_PATH, ppszArgv[nPathArgIndex]);
 
         // Copy the value if the option has one
         if (nOptValNum > 0 && (++nPathArgIndex < nArgc))
         {
-            ppszExpandedArgv[nPathArgIndex] = new WCHAR[MAX_PATH];
-            StringCchCopyW(ppszExpandedArgv[nPathArgIndex], MAX_PATH, ppszArgv[nPathArgIndex]);
+            ppszExpandedArgv[nPathArgIndex] = new TCHAR[MAX_PATH];
+            _tcscpy_s(ppszExpandedArgv[nPathArgIndex], MAX_PATH, ppszArgv[nPathArgIndex]);
         }
     }
 
@@ -165,23 +165,23 @@ bool ExpandCommandLineArguments(int nArgc, LPWSTR* ppszArgv, int& nExpandedArgc,
     // Allocate memory before doing expansion
     for (int i = nPathArgIndex; i < nExpandedArgc; ++i)
     {
-        ppszExpandedArgv[i] = new WCHAR[MAX_PATH];
+        ppszExpandedArgv[i] = new TCHAR[MAX_PATH];
     }
 
     // "dnx /path/App.dll arg1" --> "dnx --appbase /path/ /path/App.dll arg1"
     // "dnx /path/App.exe arg1" --> "dnx --appbase /path/ /path/App.exe arg1"
-    LPWSTR pszPathArg = ppszArgv[nPathArgIndex];
-    if (StringEndsWith(pszPathArg, L".exe") || StringEndsWith(pszPathArg, L".dll"))
+    LPTSTR pszPathArg = ppszArgv[nPathArgIndex];
+    if (StringEndsWith(pszPathArg, _T(".exe")) || StringEndsWith(pszPathArg, _T(".dll")))
     {
         GetParentDir(pszPathArg, szParentDir);
 
-        StringCchCopyW(ppszExpandedArgv[nPathArgIndex], MAX_PATH, L"--appbase");
-        StringCchCopyW(ppszExpandedArgv[nPathArgIndex + 1], MAX_PATH, szParentDir);
+        _tcscpy_s(ppszExpandedArgv[nPathArgIndex], MAX_PATH, _T("--appbase"));
+        _tcscpy_s(ppszExpandedArgv[nPathArgIndex + 1], MAX_PATH, szParentDir);
 
         // Copy all arguments/options as is
         for (int i = nPathArgIndex; i < nArgc; ++i)
         {
-            StringCchCopyW(ppszExpandedArgv[i + 2], MAX_PATH, ppszArgv[i]);
+            _tcscpy_s(ppszExpandedArgv[i + 2], MAX_PATH, ppszArgv[i]);
         }
 
         return true;
@@ -189,51 +189,51 @@ bool ExpandCommandLineArguments(int nArgc, LPWSTR* ppszArgv, int& nExpandedArgc,
 
     // "dnx /path/project.json run" --> "dnx --appbase /path/ Microsoft.Framework.ApplicationHost run"
     // "dnx /path/ run" --> "dnx --appbase /path/ Microsoft.Framework.ApplicationHost run"
-    WCHAR szFileName[MAX_PATH];
+    TCHAR szFileName[MAX_PATH];
     GetFileName(pszPathArg, szFileName);
-    if (StringsEqual(szFileName, L"project.json"))
+    if (StringsEqual(szFileName, _T("project.json")))
     {
         GetParentDir(pszPathArg, szParentDir);
     }
     else
     {
-        StringCchCopyW(szParentDir, MAX_PATH, pszPathArg);
+        _tcscpy_s(szParentDir, MAX_PATH, pszPathArg);
     }
 
-    StringCchCopyW(ppszExpandedArgv[nPathArgIndex], MAX_PATH, L"--appbase");
-    StringCchCopyW(ppszExpandedArgv[nPathArgIndex + 1], MAX_PATH, szParentDir);
-    StringCchCopyW(ppszExpandedArgv[nPathArgIndex + 2], MAX_PATH, L"Microsoft.Framework.ApplicationHost");
+    _tcscpy_s(ppszExpandedArgv[nPathArgIndex], MAX_PATH, _T("--appbase"));
+    _tcscpy_s(ppszExpandedArgv[nPathArgIndex + 1], MAX_PATH, szParentDir);
+    _tcscpy_s(ppszExpandedArgv[nPathArgIndex + 2], MAX_PATH, _T("Microsoft.Framework.ApplicationHost"));
 
     for (int i = nPathArgIndex + 1; i < nArgc; ++i)
     {
         // Copy all other arguments/options as is
-        StringCchCopyW(ppszExpandedArgv[i + 2], MAX_PATH, ppszArgv[i]);
+        _tcscpy_s(ppszExpandedArgv[i + 2], MAX_PATH, ppszArgv[i]);
     }
 
     return true;
 }
 
-int CallApplicationProcessMain(int argc, wchar_t* argv[])
+int CallApplicationProcessMain(int argc, LPTSTR argv[])
 {
     TCHAR szTrace[2];
-    DWORD nEnvTraceSize = GetEnvironmentVariableW(L"DNX_TRACE", szTrace, 2);
+    DWORD nEnvTraceSize = GetEnvironmentVariable(_T("DNX_TRACE"), szTrace, 2);
     if (nEnvTraceSize == 0)
     {
-        nEnvTraceSize = GetEnvironmentVariableW(L"DNX_TRACE", szTrace, 2);
+        nEnvTraceSize = GetEnvironmentVariable(_T("DNX_TRACE"), szTrace, 2);
     }
     bool m_fVerboseTrace = (nEnvTraceSize == 1);
     if (m_fVerboseTrace)
     {
-        szTrace[1] = L'\0';
-        m_fVerboseTrace = (_wcsnicmp(szTrace, L"1", 1) == 0);
+        szTrace[1] = _T('\0');
+        m_fVerboseTrace = (_tcsnicmp(szTrace, _T("1"), 1) == 0);
     }
 
     bool fSuccess = true;
     HMODULE m_hHostModule = nullptr;
 #if CORECLR_WIN
-    LPCWSTR pwzHostModuleName = L"dnx.coreclr.dll";
+    LPCTSTR pwzHostModuleName = _T("dnx.coreclr.dll");
 #else
-    LPCWSTR pwzHostModuleName = L"dnx.clr.dll";
+    LPCTSTR pwzHostModuleName = _T("dnx.clr.dll");
 #endif
 
     // Note: need to keep as ASCII as GetProcAddress function takes ASCII params
@@ -246,34 +246,34 @@ int CallApplicationProcessMain(int argc, wchar_t* argv[])
 
     // Set the DEFAULT_LIB environment variable to be the same directory
     // as the exe
-    SetEnvironmentVariable(L"DNX_DEFAULT_LIB", szCurrentDirectory);
+    SetEnvironmentVariable(_T("DNX_DEFAULT_LIB"), szCurrentDirectory);
 
     // Set the DNX_CONOSLE_HOST flag which will print exceptions
     // to stderr instead of throwing
-    SetEnvironmentVariable(L"DNX_CONSOLE_HOST", L"1");
+    SetEnvironmentVariable(_T("DNX_CONSOLE_HOST"), _T("1"));
 
     CALL_APPLICATION_MAIN_DATA data = { 0 };
     int nExpandedArgc = -1;
-    LPWSTR* ppszExpandedArgv = nullptr;
+    LPTSTR* ppszExpandedArgv = nullptr;
     bool bExpanded = ExpandCommandLineArguments(argc - 1, &(argv[1]), nExpandedArgc, ppszExpandedArgv);
     if (bExpanded)
     {
         data.argc = nExpandedArgc;
-        data.argv = const_cast<LPCWSTR*>(ppszExpandedArgv);
+        data.argv = const_cast<LPCTSTR*>(ppszExpandedArgv);
     }
     else
     {
         data.argc = argc - 1;
-        data.argv = const_cast<LPCWSTR*>(&argv[1]);
+        data.argv = const_cast<LPCTSTR*>(&argv[1]);
     }
 
     // Get application base from DNX_APPBASE environment variable
     // Note: this value can be overriden by --appbase option
     TCHAR szAppBase[MAX_PATH];
-    DWORD dwAppBase = GetEnvironmentVariableW(L"DNX_APPBASE", szAppBase, MAX_PATH);
+    DWORD dwAppBase = GetEnvironmentVariable(_T("DNX_APPBASE"), szAppBase, MAX_PATH);
     if (dwAppBase == 0)
     {
-        dwAppBase = GetEnvironmentVariableW(L"DNX_APPBASE", szAppBase, MAX_PATH);
+        dwAppBase = GetEnvironmentVariable(_T("DNX_APPBASE"), szAppBase, MAX_PATH);
     }
     if (dwAppBase != 0)
     {
@@ -282,7 +282,7 @@ int CallApplicationProcessMain(int argc, wchar_t* argv[])
 
     for (int i = 0; i < data.argc; ++i)
     {
-        if ((i < data.argc - 1) && StringsEqual(data.argv[i], L"--appbase"))
+        if ((i < data.argc - 1) && StringsEqual(data.argv[i], _T("--appbase")))
         {
             data.applicationBase = data.argv[i + 1];
         }
@@ -294,43 +294,43 @@ int CallApplicationProcessMain(int argc, wchar_t* argv[])
     }
 
     // Prevent coreclr native bootstrapper from failing with relative appbase
-    WCHAR szFullAppBase[MAX_PATH];
-    DWORD dwFullAppBase = GetFullPathNameW(data.applicationBase, MAX_PATH, szFullAppBase, nullptr);
+    TCHAR szFullAppBase[MAX_PATH];
+    DWORD dwFullAppBase = GetFullPathName(data.applicationBase, MAX_PATH, szFullAppBase, nullptr);
     if (!dwFullAppBase)
     {
-        ::wprintf_s(L"Failed to get full path of application base: %s\r\n", data.applicationBase);
+        ::_tprintf_s(_T("Failed to get full path of application base: %s\r\n"), data.applicationBase);
         exitCode = 1;
         goto Finished;
     }
     else if (dwFullAppBase > MAX_PATH)
     {
-        ::wprintf_s(L"Full path of application base is too long\r\n");
+        ::_tprintf_s(_T("Full path of application base is too long\r\n"));
         exitCode = 1;
         goto Finished;
     }
     data.applicationBase = szFullAppBase;
 
-    m_hHostModule = ::LoadLibraryExW(pwzHostModuleName, NULL, LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
+    m_hHostModule = ::LoadLibraryEx(pwzHostModuleName, NULL, LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
     if (!m_hHostModule)
     {
         if (m_fVerboseTrace)
-            ::wprintf_s(L"Failed to load: %s\r\n", pwzHostModuleName);
+            ::_tprintf_s(_T("Failed to load: %s\r\n"), pwzHostModuleName);
         m_hHostModule = nullptr;
         goto Finished;
     }
     if (m_fVerboseTrace)
-        ::wprintf_s(L"Loaded Module: %s\r\n", pwzHostModuleName);
+        ::_tprintf_s(_T("Loaded Module: %s\r\n"), pwzHostModuleName);
 
     pfnCallApplicationMain = (FnCallApplicationMain)::GetProcAddress(m_hHostModule, pszCallApplicationMainName);
     if (!pfnCallApplicationMain)
     {
         if (m_fVerboseTrace)
-            ::wprintf_s(L"Failed to find function %S in %s\n", pszCallApplicationMainName, pwzHostModuleName);
+            ::_tprintf_s(_T("Failed to find function %S in %s\n"), pszCallApplicationMainName, pwzHostModuleName);
         fSuccess = false;
         goto Finished;
     }
     if (m_fVerboseTrace)
-        printf_s("Found DLL Export: %s\r\n", pszCallApplicationMainName);
+        ::_tprintf_s(_T("Found DLL Export: %s\r\n"), pszCallApplicationMainName);
 
     HRESULT hr = pfnCallApplicationMain(&data);
     if (SUCCEEDED(hr))
@@ -377,12 +377,12 @@ int wmain(int argc, wchar_t* argv[])
     // Check for the debug flag before doing anything else
     for (int i = 0; i < argc; i++)
     {
-        if (StringsEqual(argv[i], L"--debug"))
+        if (StringsEqual(argv[i], _T("--debug")))
         {
             if (!IsDebuggerPresent())
             {
-                ::wprintf_s(L"Process Id: %ld\r\n", GetCurrentProcessId());
-                ::wprintf_s(L"Waiting for the debugger to attach...\r\n");
+                ::_tprintf_s(_T("Process Id: %ld\r\n"), GetCurrentProcessId());
+                ::_tprintf_s(_T("Waiting for the debugger to attach...\r\n"));
 
                 // Do not use getchar() like in corerun because it doesn't work well with remote sessions
                 while (!IsDebuggerPresent())
@@ -390,7 +390,7 @@ int wmain(int argc, wchar_t* argv[])
                     Sleep(250);
                 }
 
-                ::wprintf_s(L"Debugger attached.\r\n");
+                ::_tprintf_s(_T("Debugger attached.\r\n"));
             }
         }
     }
--- src/dnx/dnx.h
+++ src/dnx/dnx.h
@@ -1,10 +1,10 @@
 
 typedef struct CALL_APPLICATION_MAIN_DATA
 {
-    LPCWSTR applicationBase; // application base of managed domain
-    LPCWSTR runtimeDirectory; // path to runtime helper directory
+    LPCTSTR applicationBase; // application base of managed domain
+    LPCTSTR runtimeDirectory; // path to runtime helper directory
     int argc; // Number of args in argv
-    LPCWSTR* argv; // Array of arguments
+    LPCTSTR* argv; // Array of arguments
     int exitcode; // Exit code from Managed Application
 } *PCALL_APPLICATION_MAIN_DATA;
 
