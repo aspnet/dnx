07c61675a34adf3219a3d51373c89c66f7c33c25
anurse <andrew@andrewnurse.net>
2015-03-30 11:39:18 -0700

    remove tpa changes

--- src/dnx.coreclr/tpa.cpp
+++ src/dnx.coreclr/tpa.cpp
@@ -6,7 +6,7 @@
 
 BOOL CreateTpaBase(LPCTSTR** ppNames, size_t* pcNames, bool bNative)
 {
-    const size_t count = 35;
+    const size_t count = 34;
     LPCTSTR* pArray = new LPCTSTR[count];
 
     if (bNative)
@@ -25,27 +25,26 @@ BOOL CreateTpaBase(LPCTSTR** ppNames, size_t* pcNames, bool bNative)
         pArray[11] = _T("System.Console.ni.dll");
         pArray[12] = _T("System.Core.ni.dll");
         pArray[13] = _T("System.Diagnostics.Debug.ni.dll");
-        pArray[14] = _T("System.Diagnostics.Tracing.ni.dll");
-        pArray[15] = _T("System.Globalization.ni.dll");
-        pArray[16] = _T("System.IO.ni.dll");
-        pArray[17] = _T("System.IO.FileSystem.ni.dll");
-        pArray[18] = _T("System.IO.FileSystem.Primitives.ni.dll");
-        pArray[19] = _T("System.Linq.ni.dll");
-        pArray[20] = _T("System.Reflection.ni.dll");
-        pArray[21] = _T("System.Reflection.Extensions.ni.dll");
-        pArray[22] = _T("System.Reflection.Primitives.ni.dll");
-        pArray[23] = _T("System.Resources.ResourceManager.ni.dll");
-        pArray[24] = _T("System.Runtime.ni.dll");
-        pArray[25] = _T("System.Runtime.Extensions.ni.dll");
-        pArray[26] = _T("System.Runtime.Handles.ni.dll");
-        pArray[27] = _T("System.Runtime.InteropServices.ni.dll");
-        pArray[28] = _T("System.Runtime.Loader.ni.dll");
-        pArray[29] = _T("System.Text.Encoding.ni.dll");
-        pArray[30] = _T("System.Text.Encoding.Extensions.ni.dll");
-        pArray[31] = _T("System.Threading.ni.dll");
-        pArray[32] = _T("System.Threading.Overlapped.ni.dll");
-        pArray[33] = _T("System.Threading.Tasks.ni.dll");
-        pArray[34] = _T("System.Threading.ThreadPool.ni.dll");
+        pArray[14] = _T("System.Globalization.ni.dll");
+        pArray[15] = _T("System.IO.ni.dll");
+        pArray[16] = _T("System.IO.FileSystem.ni.dll");
+        pArray[17] = _T("System.IO.FileSystem.Primitives.ni.dll");
+        pArray[18] = _T("System.Linq.ni.dll");
+        pArray[19] = _T("System.Reflection.ni.dll");
+        pArray[20] = _T("System.Reflection.Extensions.ni.dll");
+        pArray[21] = _T("System.Reflection.Primitives.ni.dll");
+        pArray[22] = _T("System.Resources.ResourceManager.ni.dll");
+        pArray[23] = _T("System.Runtime.ni.dll");
+        pArray[24] = _T("System.Runtime.Extensions.ni.dll");
+        pArray[25] = _T("System.Runtime.Handles.ni.dll");
+        pArray[26] = _T("System.Runtime.InteropServices.ni.dll");
+        pArray[27] = _T("System.Runtime.Loader.ni.dll");
+        pArray[28] = _T("System.Text.Encoding.ni.dll");
+        pArray[29] = _T("System.Text.Encoding.Extensions.ni.dll");
+        pArray[30] = _T("System.Threading.ni.dll");
+        pArray[31] = _T("System.Threading.Overlapped.ni.dll");
+        pArray[32] = _T("System.Threading.Tasks.ni.dll");
+        pArray[33] = _T("System.Threading.ThreadPool.ni.dll");
     }
     else
     {
@@ -63,27 +62,26 @@ BOOL CreateTpaBase(LPCTSTR** ppNames, size_t* pcNames, bool bNative)
         pArray[11] = _T("System.Console.dll");
         pArray[12] = _T("System.Core.dll");
         pArray[13] = _T("System.Diagnostics.Debug.dll");
-        pArray[14] = _T("System.Diagnostics.Tracing.dll");
-        pArray[15] = _T("System.Globalization.dll");
-        pArray[16] = _T("System.IO.dll");
-        pArray[17] = _T("System.IO.FileSystem.dll");
-        pArray[18] = _T("System.IO.FileSystem.Primitives.dll");
-        pArray[19] = _T("System.Linq.dll");
-        pArray[20] = _T("System.Reflection.dll");
-        pArray[21] = _T("System.Reflection.Extensions.dll");
-        pArray[22] = _T("System.Reflection.Primitives.dll");
-        pArray[23] = _T("System.Resources.ResourceManager.dll");
-        pArray[24] = _T("System.Runtime.dll");
-        pArray[25] = _T("System.Runtime.Extensions.dll");
-        pArray[26] = _T("System.Runtime.Handles.dll");
-        pArray[27] = _T("System.Runtime.InteropServices.dll");
-        pArray[28] = _T("System.Runtime.Loader.dll");
-        pArray[29] = _T("System.Text.Encoding.dll");
-        pArray[30] = _T("System.Text.Encoding.Extensions.dll");
-        pArray[31] = _T("System.Threading.dll");
-        pArray[32] = _T("System.Threading.Overlapped.dll");
-        pArray[33] = _T("System.Threading.Tasks.dll");
-        pArray[34] = _T("System.Threading.ThreadPool.dll");
+        pArray[14] = _T("System.Globalization.dll");
+        pArray[15] = _T("System.IO.dll");
+        pArray[16] = _T("System.IO.FileSystem.dll");
+        pArray[17] = _T("System.IO.FileSystem.Primitives.dll");
+        pArray[18] = _T("System.Linq.dll");
+        pArray[19] = _T("System.Reflection.dll");
+        pArray[20] = _T("System.Reflection.Extensions.dll");
+        pArray[21] = _T("System.Reflection.Primitives.dll");
+        pArray[22] = _T("System.Resources.ResourceManager.dll");
+        pArray[23] = _T("System.Runtime.dll");
+        pArray[24] = _T("System.Runtime.Extensions.dll");
+        pArray[25] = _T("System.Runtime.Handles.dll");
+        pArray[26] = _T("System.Runtime.InteropServices.dll");
+        pArray[27] = _T("System.Runtime.Loader.dll");
+        pArray[28] = _T("System.Text.Encoding.dll");
+        pArray[29] = _T("System.Text.Encoding.Extensions.dll");
+        pArray[30] = _T("System.Threading.dll");
+        pArray[31] = _T("System.Threading.Overlapped.dll");
+        pArray[32] = _T("System.Threading.Tasks.dll");
+        pArray[33] = _T("System.Threading.ThreadPool.dll");
     }
 
     *ppNames = pArray;
