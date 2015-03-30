c701f28f8c51e33085c69c1b03ad564cc5b37546
David Fowler <davidfowl@gmail.com>
2015-03-29 19:26:12 -0700

    Fixed Equals for DiagnosticsMessage

--- src/Microsoft.Framework.DesignTimeHost/Models/OutgoingMessages/DiagnosticsMessage.cs
+++ src/Microsoft.Framework.DesignTimeHost/Models/OutgoingMessages/DiagnosticsMessage.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Framework.Internal;
@@ -58,14 +59,39 @@ namespace Microsoft.Framework.DesignTimeHost.Models.OutgoingMessages
             var other = obj as DiagnosticsMessage;
 
             return other != null &&
-                Enumerable.SequenceEqual(Errors, other.Errors) &&
-                Enumerable.SequenceEqual(Warnings, other.Warnings) &&
-                Framework == other.Framework;
+                Enumerable.SequenceEqual(Errors, other.Errors, Comparer.Default) &&
+                Enumerable.SequenceEqual(Warnings, other.Warnings, Comparer.Default) &&
+                object.Equals(Framework, other.Framework);
         }
 
         public override int GetHashCode()
         {
             return base.GetHashCode();
+        }
+
+        private class Comparer : IEqualityComparer<ICompilationMessage>
+        {
+            public static readonly Comparer Default = new Comparer();
+
+            public bool Equals(ICompilationMessage x, ICompilationMessage y)
+            {
+                return x.StartLine == y.StartLine &&
+                       x.StartColumn == y.StartColumn &&
+                       string.Equals(x.Message, y.Message, StringComparison.OrdinalIgnoreCase) &&
+                       string.Equals(x.SourceFilePath, y.SourceFilePath, StringComparison.OrdinalIgnoreCase);
+            }
+
+            public int GetHashCode(ICompilationMessage obj)
+            {
+                var hash = obj.StartLine.GetHashCode() ^ obj.StartColumn.GetHashCode() ^ obj.Message.GetHashCode();
+
+                if (obj.SourceFilePath != null)
+                {
+                    hash ^= obj.SourceFilePath.GetHashCode();
+                }
+
+                return hash;
+            }
         }
     }
 }
