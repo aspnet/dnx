5b4d445729c14c87327f1e3d47ad8fbfc564bf1d
Troy Dai <troy.dai@outlook.com>
2015-04-15 10:51:45 -0700

    React to TestUtil change

--- test/Microsoft.Framework.PackageManager.FunctionalTests/DnuListTests.cs
+++ test/Microsoft.Framework.PackageManager.FunctionalTests/DnuListTests.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using Microsoft.Framework.FunctionalTestUtils;
+using Microsoft.Framework.CommonTestUtils;
 using Newtonsoft.Json;
 using Xunit;
 
