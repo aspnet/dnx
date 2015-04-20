ff4d042d4022b1d183bc36b819d561536ebcde12
Wei Wang <chengtian.wang@gmail.com>
2015-04-20 11:30:24 -0700

    Add compatibility check for "dnxcore50 -> portable-net45+win8"

--- src/Microsoft.Framework.PackageManager/Utils/LockFileUtils.cs
+++ src/Microsoft.Framework.PackageManager/Utils/LockFileUtils.cs
@@ -182,6 +182,16 @@ namespace Microsoft.Framework.PackageManager.Utils
                         framework, item => item.TargetFramework)?.Packages.ToList();
                     var runtimeAssemblies = NuGetFrameworkUtility.GetNearest(packageReader.GetReferenceItems(),
                         framework, item => item.TargetFramework)?.Items.ToList();
+
+                    // The compatibility mapping "dnxcore50 -> portable-net45+win8" is missing in NuGet core libs
+                    // We need to do that check here
+                    if (runtimeAssemblies == null &&
+                        framework == FrameworkConstants.CommonFrameworks.DnxCore50)
+                    {
+                        runtimeAssemblies = packageReader.GetReferenceItems()
+                            .FirstOrDefault(x => string.Equals(x.TargetFramework.Profile, "Profile7"))?.Items.ToList();
+                    }
+
                     var frameworkAssembliesGroup = NuGetFrameworkUtility.GetNearest(packageReader.GetFrameworkItems(),
                         framework, item => item.TargetFramework);
 
