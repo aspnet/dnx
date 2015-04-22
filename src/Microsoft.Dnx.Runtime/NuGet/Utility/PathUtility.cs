306e7383e629c36d62531781b557477ed79fc4de
Brennan <brecon@microsoft.com>
2015-04-22 09:28:26 -0700

    Don't use Uri class for GetRelativePath

--- src/Microsoft.Dnx.Runtime/NuGet/Utility/PathUtility.cs
+++ src/Microsoft.Dnx.Runtime/NuGet/Utility/PathUtility.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.IO;
+using Microsoft.Dnx.Runtime;
 
 namespace NuGet
 {
@@ -72,7 +73,72 @@ namespace NuGet
         /// </summary>
         public static string GetRelativePath(string path1, string path2, char separator)
         {
-            return UriUtility.GetPath(GetRelativeUri(path1, path2), separator);
+            if (path1 == null)
+            {
+                throw new ArgumentNullException("path1");
+            }
+
+            if (path2 == null)
+            {
+                throw new ArgumentNullException("path2");
+            }
+
+            StringComparison compare;
+            if (RuntimeEnvironmentHelper.IsWindows)
+            {
+                compare = StringComparison.OrdinalIgnoreCase;
+            }
+            else
+            {
+                compare = StringComparison.Ordinal;
+            }
+
+            var index = 0;
+            var path1Segments = path1.Split(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+            var path2Segments = path2.Split(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+            // if path1 does not end with / it is assumed the end is not a directory
+            // we will assume that is isn't a directory by ignoring the last split
+            var len1 = path1Segments.Length - 1;
+            var len2 = path2Segments.Length;
+
+            var min = Math.Min(len1, len2);
+            while (min > index)
+            {
+                if (string.Compare(path1Segments[index], path2Segments[index], compare) != 0)
+                {
+                    break;
+                }
+                else if ((len1 == index && len2 > index + 1) || (len1 > index && len2 == index + 1))
+                {
+                    break;
+                }
+                ++index;
+            }
+
+            var path = "";
+
+            // check if path2 ends with a non-directory separator and if path1 has the same non-directory at the end
+            if (len1 + 1 == len2 && !string.IsNullOrEmpty(path1Segments[index]) &&
+                string.Compare(path1Segments[index], path2Segments[index], compare) == 0)
+            {
+                return path;
+            }
+
+            for (var i = index; len1 > i; ++i)
+            {
+                path += ".." + separator;
+            }
+            for (var i = index; len2 - 1 > i; ++i)
+            {
+                path += path2Segments[i] + separator;
+            }
+            // if path2 doesn't end with an empty string it means it ended with a non-directory name, so we add it back
+            if (!string.IsNullOrEmpty(path2Segments[len2 - 1]))
+            {
+                path += path2Segments[len2 - 1];
+            }
+
+            return path;
         }
 
         /// <summary>
