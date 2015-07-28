39546e47acbe31857693da1de3ca9c80382bc15f
Pranav K <prkrishn@hotmail.com>
2015-07-28 12:56:22 -0700

    Updating json files to pin versions and build.cmd to pin KoreBuild and DNX

--- build.cmd
+++ build.cmd
@@ -16,17 +16,21 @@ copy %CACHED_NUGET% .nuget\nuget.exe > nul
 
 :restore
 IF EXIST packages\KoreBuild goto run
-.nuget\NuGet.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
+IF DEFINED BUILDCMD_RELEASE (
+	.nuget\NuGet.exe install KoreBuild -version 0.2.1-%BUILDCMD_RELEASE% -ExcludeVersion -o packages -nocache -pre
+) ELSE (
+	.nuget\NuGet.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
+)
 .nuget\NuGet.exe install Sake -version 0.2 -o packages -ExcludeVersion
 
-IF "%1" == "rebuild-package" goto run
-
-IF "%SKIP_DNX_INSTALL%"=="1" (
-    REM On the CI, don't upgrade since the previous installed DNVM is already there.
-    CALL packages\KoreBuild\build\dnvm use default -runtime CLR -arch x86
+IF "%SKIP_DNX_INSTALL%"=="1" goto run
+IF DEFINED BUILDCMD_RELEASE (
+	CALL packages\KoreBuild\build\dnvm install 1.0.0-%BUILDCMD_RELEASE% -runtime CLR -arch x86 -a default
 ) ELSE (
-    CALL packages\KoreBuild\build\dnvm upgrade -runtime CLR -arch x86
+	CALL packages\KoreBuild\build\dnvm upgrade -runtime CLR -arch x86 
 )
+CALL packages\KoreBuild\build\dnvm install default -runtime CoreCLR -arch x86
 
 :run
+CALL packages\KoreBuild\build\dnvm use default -runtime CLR -arch x86
 packages\Sake\tools\Sake.exe -I packages\KoreBuild\build -f makefile.shade %*
