fb88afaeed702b1af730f49b49f696b3f8e8b68f
BrennanConroy <brecon@microsoft.com>
2015-08-21 23:11:56 -0700

    Fix tests on OS X

--- test/Microsoft.Dnx.CommonTestUtils/TestUtils.cs
+++ test/Microsoft.Dnx.CommonTestUtils/TestUtils.cs
@@ -30,6 +30,16 @@ namespace Microsoft.Dnx.CommonTestUtils
         {
             var tempDirPath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
             Directory.CreateDirectory(tempDirPath);
+
+            if (string.Equals(RuntimeEnvironmentHelper.RuntimeEnvironment.OperatingSystem, "Darwin"))
+            {
+                // Resolves issues on Mac where GetTempPath gives /var and GetCurrentDirectory gives /private/var
+                var currentDirectory = Directory.GetCurrentDirectory();
+                Directory.SetCurrentDirectory(tempDirPath);
+                tempDirPath = Directory.GetCurrentDirectory();
+                Directory.SetCurrentDirectory(currentDirectory);
+            }
+
             return tempDirPath;
         }
 
