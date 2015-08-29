3fb650e1840a99e23f2ffca90f9e8e38e067a43e
John Luo <jt.luo@mail.utoronto.ca>
2015-08-28 17:33:00 -0700

    changed to using string interpolation in BuildManager.cs

--- src/Microsoft.Dnx.Tooling/Building/BuildManager.cs
+++ src/Microsoft.Dnx.Tooling/Building/BuildManager.cs
@@ -413,7 +413,7 @@ namespace Microsoft.Dnx.Tooling
 
         private static string GetPackagePath(Runtime.Project project, string outputPath, bool symbols = false)
         {
-            string fileName = project.Name + "." + project.Version.ToString() + (symbols ? ".symbols" : "") + NuGet.Constants.PackageExtension;
+            string fileName = symbols ? $"{project.Name}.{project.Version}.symbols{NuGet.Constants.PackageExtension}" : $"{project.Name}.{project.Version}{NuGet.Constants.PackageExtension}";
             return Path.Combine(outputPath, fileName);
         }
 
