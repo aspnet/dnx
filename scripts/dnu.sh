cbd10a4a418ec175d7db3d08901e18a6e9b1f29a
Alexander Köplinger <alex.koeplinger@outlook.com>
2015-09-03 00:00:35 +0200

    Add MONO_THREADS_PER_CPU to dnu.sh to workaround restore timeouts on Mono
    This is troubling a lot of people and I honestly don't expect it to be fixed for Mono 4.2.
    Adding the workaround to dnu.sh seems like a better solution for now instead of telling everyone to add it themselves.

--- scripts/dnu.sh
+++ scripts/dnu.sh
@@ -8,5 +8,7 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
+# work around restore timeouts on Mono
+[ -z "$MONO_THREADS_PER_CPU" ] && export MONO_THREADS_PER_CPU=50
 
 exec "$DIR/dnx" "$DIR/lib/Microsoft.Dnx.Tooling/Microsoft.Dnx.Tooling.dll" "$@"
