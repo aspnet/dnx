374170ae0b63ad4c4e212f05bfd27cc90fec1bc9
Pranav K <prkrishn@hotmail.com>
2015-09-10 17:01:00 -0700

    Adding NeutralResourcesLanguageAttribute

--- src/Microsoft.Dnx.Tooling/Properties/AssemblyInfo.cs
+++ src/Microsoft.Dnx.Tooling/Properties/AssemblyInfo.cs
@@ -2,7 +2,10 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Microsoft.Dnx.Tooling.Tests")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
