91d1193359599fa83888a958e642d7b345754767
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2015-09-11 15:01:28 -0700

    make batchcopy.sh more defensive to fix linux/mac build

--- build/batchcopy.sh
+++ build/batchcopy.sh
@@ -10,5 +10,8 @@ if [ ! -d "$dst" ]; then
   mkdir -p $dst
 fi
 
-cp $src $dst
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+# Copy the files, if they exist
+if ls $src 1> /dev/null 2>&1; then
+    cp $src $dst
+    rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+fi
