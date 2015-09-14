255999f7be7008e153009dcd966c2ee690e648cb
moozzyk <pawelka@microsoft.com>
2015-09-14 12:58:00 -0700

    Passing context from native bootstrapper coreclr windows

--- src/Microsoft.Dnx.Host.CoreClr/DomainManager.cs
+++ src/Microsoft.Dnx.Host.CoreClr/DomainManager.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 using System.Security;
 using Microsoft.Dnx.Host;
@@ -11,8 +12,21 @@ using Microsoft.Dnx.Runtime.Common.Impl;
 [SecurityCritical]
 sealed class DomainManager
 {
+    // this structure is used to pass data from native code
+    // and needs to be in sync with its native counterpart
+    [StructLayout(LayoutKind.Sequential)]
+    private unsafe struct NativeBootstrapperContext
+    {
+        public char* OperatingSystem;
+        public char* OsVersion;
+        public char* Architecture;
+        public char* RuntimeDirectory;
+        public char* ApplicationBase;
+        public bool HandleExceptions;
+    }
+
     [SecurityCritical]
-    unsafe static int Execute(int argc, char** argv)
+    unsafe static int Execute(int argc, char** argv, NativeBootstrapperContext* context)
     {
         Logger.TraceInformation($"[{nameof(DomainManager)}] Using CoreCLR");
 
@@ -25,9 +39,20 @@ sealed class DomainManager
 
         try
         {
+            var bootstrapperContext = new BootstrapperContext();
+
+            bootstrapperContext.OperatingSystem = new string(context->OperatingSystem);
+            bootstrapperContext.OsVersion = new string(context->OsVersion);
+            bootstrapperContext.Architecture = new string(context->Architecture);
+            bootstrapperContext.RuntimeDirectory = new string(context->RuntimeDirectory);
+            bootstrapperContext.ApplicationBase = new string(context->ApplicationBase);
+            bootstrapperContext.TargetFramework = new FrameworkName(FrameworkNames.LongNames.DnxCore, new Version(5, 0));
+            bootstrapperContext.HandleExceptions = context->HandleExceptions;
+
             return RuntimeBootstrapper.Execute(
                 arguments,
-                new FrameworkName(FrameworkNames.LongNames.DnxCore, new Version(5, 0)));
+                bootstrapperContext.TargetFramework,
+                bootstrapperContext);
         }
         catch (Exception ex)
         {
