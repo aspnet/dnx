07ba4d79c24be2b1cb152f7ff49e2414dd7b8ddf
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2015-09-16 15:44:45 -0700

    Enabling NuGetPackageVerifier

--- src/Microsoft.Dnx.Compilation/Properties/AssemblyInfo.cs
+++ src/Microsoft.Dnx.Compilation/Properties/AssemblyInfo.cs
@@ -1,3 +1,10 @@
-﻿using System.Runtime.CompilerServices;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Microsoft.Dnx.Compilation.Tests")]
+[assembly: AssemblyMetadata("Serviceable", "True")]
+[assembly: NeutralResourcesLanguage("en-US")]
