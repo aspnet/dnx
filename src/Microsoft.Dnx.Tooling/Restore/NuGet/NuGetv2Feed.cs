7112c27b8a458a5fb74fafb52da797942007b0b0
dagood <dagood@microsoft.com>
2015-10-08 14:02:53 -0500

    Stop using Title as a fallback for Id. This is only sometimes a useful fallback, and in some cases it actually prevents success: falling back to the given Id would have worked.

--- src/Microsoft.Dnx.Tooling/Restore/NuGet/NuGetv2Feed.cs
+++ src/Microsoft.Dnx.Tooling/Restore/NuGet/NuGetv2Feed.cs
@@ -208,9 +208,10 @@ namespace Microsoft.Dnx.Tooling.Restore.NuGet
             return new PackageInfo
             {
                 // If 'Id' element exist, use its value as accurate package Id
-                // Otherwise, use the value of 'title' if it exist
-                // Use the given Id as final fallback if all elements above don't exist
-                Id = idElement?.Value ?? titleElement?.Value ?? id,
+                // Use the given Id as fallback if Id does not exist.
+                // Sometimes 'title' element is the same as Id and can be used as a fallback,
+                // but this is not always the case so don't use it.
+                Id = idElement?.Value ?? id,
                 Version = SemanticVersion.Parse(properties.Element(_xnameVersion).Value),
                 ContentUri = element.Element(_xnameContent).Attribute("src").Value,
                 Listed = listed
