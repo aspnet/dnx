3659ba5a113307f0866303325c8caf122a8ee736
Pranav K <prkrishn@hotmail.com>
2015-11-03 18:31:24 -0800

    Revert "Merge branch 'release' into dev"
    This reverts commit 7b7fd6b3339256b3f69bc0cdfd4521ef318767a0.

--- samples/HelloWorld/compiler/preprocess/HelloMetaProgramming.cs
+++ samples/HelloWorld/compiler/preprocess/HelloMetaProgramming.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 //using Microsoft.CodeAnalysis;
 //using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.Extensions.PlatformAbstractions;
 
 namespace HelloWorld.Compiler.Preprocess
 {
