f965937ab007d738d1263fba04b26497537b2b20
Pavel Krymets <pavel@krymets.com>
2015-11-04 08:10:58 -0800

    Fix unit test

--- misc/AppHostServicesProjects/src/GetExports/Program.cs
+++ misc/AppHostServicesProjects/src/GetExports/Program.cs
@@ -9,7 +9,7 @@ namespace GetExports
         {
             var exporter = CompilationServices.Default.LibraryExporter;
             var projectExport = exporter.GetAllExports("GetExports");
-            var packageExport = exporter.GetAllExports("Microsoft.Extension.Compilation.Abstractions");
+            var packageExport = exporter.GetAllExports("Microsoft.Extensions.Compilation.Abstractions");
             
             Console.WriteLine($"Project: {projectExport.MetadataReferences[0].Name}");
             Console.WriteLine($"Package: {packageExport.MetadataReferences[0].Name}");
